1eff2274aa5d533c9dc25e1cc4b06ef4d20a9da7
Anders Markvardsen <anders.markvardsen@stfc.ac.uk>
2018-05-24 10:04:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='fitbenchmarking',
-      version='0',
+      version='0.1dev1',
       description='Fit benchmarking (this needs updating)',
       url='http://github.com/mantidproject/fitbenchmarking',
       license='GPL-3.0',
