8d343c0f99781b982784e4d5997ca85994d90cf8
Patrick Odagiu <33397154+bb511@users.noreply.github.com>
2018-07-06 15:55:30 +0100

    Test if the file names for neutron and nist data are imported correctly

---
+++ fitbenchmarking/test/test_import.py
@@ -0,0 +1,70 @@
+# Unit testing the file importing functions
+import unittest
+import os
+
+# DELETE RELATIVE PATH WHEN GIT TESTS ARE ENABLED
+import sys
+sys.path.insert(0, 'D:\\fitbenchmarking\\fitbenchmarking')
+
+from fitting_benchmarking import get_nist_problem_files
+from fitting_benchmarking import get_data_groups
+# from fitting_benchmarking import get_muon_problem_files
+# from fitting_benchmarking import get_cutest_problem_files
+
+class GetProblemFilesTest(unittest.TestCase):
+
+	def basePath(self):
+		return 'arbitrary_path' + os.sep + 'fitbenchmarking' + os.sep + 'benchmark_problems' + os.sep
+
+	def test_get_nist_problem_files(self):
+
+            base_path_nist = self.basePath()
+            nist_problems = [['Misra1a.dat', 'Chwirut2.dat', 'Chwirut1.dat', 'Lanczos3.dat',
+                              'Gauss1.dat', 'Gauss2.dat', 'DanWood.dat', 'Misra1b.dat'],
+                             ['Kirby2.dat', 'Hahn1.dat','MGH17.dat', 'Lanczos1.dat', 'Lanczos2.dat', 
+                              'Gauss3.dat','Misra1c.dat', 'Misra1d.dat','ENSO.dat'], 
+                             ['MGH09.dat', 'Thurber.dat', 'BoxBOD.dat', 'Rat42.dat',
+                              'MGH10.dat', 'Eckerle4.dat', 'Rat43.dat', 'Bennett5.dat']]
+
+            paths_to_nist_problems = []                  
+        for nist_level_group in nist_problems:
+        	paths_to_level_group = [base_path_nist + nist_prob_name for nist_prob_name in nist_level_group]
+        	paths_to_nist_problems.append(paths_to_level_group)
+
+        self.assertListEqual(get_nist_problem_files(base_path_nist), paths_to_nist_problems)
+
+    def test_get_nist_problem_files_fail_empty_arrays(self):
+
+    	base_path_nist = self.basePath()
+    	self.assertListEqual(get_nist_problem_files(base_path_nist), [[],[],[]])
+
+    def test_get_data_groups(self):
+    	
+    	base_path_neutron = self.basePath()
+    	neutron_problems = [['ENGINX193749_calibration_peak19.txt', 'ENGINX193749_calibration_peak20.txt',
+							 'ENGINX193749_calibration_peak23.txt', 'ENGINX193749_calibration_peak5.txt',
+							 'ENGINX193749_calibration_peak6.txt', 
+							 'ENGINX236516_vanadium_bank1_10brk.txt', 'ENGINX236516_vanadium_bank1_20brk.txt',
+							 'EUS14188-90_Gaussian_peaks_1.txt', 'EUS14188-90_Gaussian_peaks_2.txt',
+							 'GEMpeak1.txt',
+							 'WISH17701_peak1.txt', 'WISH17701_peak2.txt',
+							 'WISH17701_peak3.txt', 'WISH17701_peak4.txt',
+							 'WISH17701_peak5.txt', 'WISH17701_peak6.txt',
+							 'WISH17701_peak7.txt', 'WISH17701_peak8.txt',
+							 'WISH17701_peak9.txt']]
+
+        paths_to_neutron_problems = []                  
+    	for neutron_level_group in neutron_problems:
+    		paths_to_level_group = [base_path_neutron + neutron_prob_name for neutron_prob_name in neutron_level_group]
+    		paths_to_neutron_problems.append(paths_to_level_group)
+
+        self.assertListEqual(get_data_groups(base_path_neutron), paths_to_neutron_problems)
+
+    def test_get_data_groups_fail_empty_arrays(self):
+
+    	base_path_neutron = self.basePath()
+    	self.assertListEqual(get_data_groups(base_path_neutron), [[],[],[]])
+
+
+if __name__ == "__main__":
+    unittest.main()
