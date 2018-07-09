63fe7ec02d60ee445576da26ffbf987a614771c0
Patrick <patrick.odagiu>
2018-07-09 22:28:20 +0100

    Preparing fitting benchmarking one problem unit tests

---
+++ fitbenchmarking/test/FittingBenchmarkingOneProblem.py
@@ -0,0 +1,34 @@
+from __future__ import (absolute_import, division, print_function)
+
+import unittest
+import os
+
+# DELETE RELATIVE PATH WHEN GIT TESTS ARE ENABLED
+import sys
+test_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(os.path.normpath(test_dir))
+sys.path.insert(0, parent_dir)
+
+from fitting_benchmarking import do_fitting_benchmark_one_problem
+from fitting_benchmarking import prepare_wks_cost_function
+from fitting_benchmarking import get_function_definitions
+from fitting_benchmarking import run_fit
+from fitting_benchmarking import SplitByString
+
+
+class FittingBenchmarkingOneProblem(unittest.TestCase):
+
+    def test_do_fitting_benchmarking_one_problem(self):
+
+
+    def test_prepare_wks_cost_function(self):
+
+
+    def test_get_function_definitions(self):
+
+
+    def test_run_fit(self):
+
+
+    def test_SplitByString(self):
+
