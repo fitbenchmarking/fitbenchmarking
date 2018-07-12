f17e3e1b1329e0a041fa75c8ca3396ba1cc5446c
Patrick Odagiu <33397154+bb511@users.noreply.github.com>
2018-07-12 15:18:51 +0100

    Added more tests and fixed formatting issues

--- fitbenchmarking/test/FittingBenchmarkingOneProblemTest.py
+++ fitbenchmarking/test/FittingBenchmarkingOneProblemTest.py
@@ -4,6 +4,7 @@ import unittest
 import os
 import mantid.simpleapi as msapi
 import numpy as np
+import decimal
 
 # DELETE RELATIVE PATH WHEN GIT TESTS ARE ENABLED
 import sys
@@ -22,15 +23,6 @@ import test_problem
 
 
 class FittingBenchmarkingOneProblem(unittest.TestCase):
-
-    def MockProblemsDir(self):
-        ''' Helper function that returns the path ../test/mock_problems/ '''
-
-        current_dir = os.path.dirname(os.path.realpath(__file__))
-        mock_problems_dir = os.path.join(current_dir, 'mock_problems')
-
-        return mock_problems_dir
-
 
     def EnginxDataPath(self):
         ''' Helper function that returns the path ../benchmark_problems/ '''
@@ -75,7 +67,8 @@ class FittingBenchmarkingOneProblem(unittest.TestCase):
             the neutron data file ENGINX193749_calibration_spec651.nxs '''
 
         # Obtained on mantid v3.12.1
-        mock_problems_dir = self.MockProblemsDir()
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        mock_problems_dir = os.path.join(current_dir, 'mock_problems')
         reference_fit_wks_path = os.path.join(mock_problems_dir, 'data_files',
                                            'fitWorkspace_enginxPeak19.nxs')
 
@@ -142,27 +135,27 @@ class FittingBenchmarkingOneProblem(unittest.TestCase):
     def test_do_fitting_benchmark_one_problem_neutron(self):
 
         prob = self.NeutronProblem()
-        minimizers = ['Levenberg-Marquardt']
-        use_errors = True
-        count = 0
-        previous_name = "none"
-        reference_fit_wks_path = self.NeutronProblemReferenceFitWks()
-        fit_wks_actual = msapi.Load(reference_fit_wks_path)
 
         result_actual = test_result.FittingTestResult()
         result_actual.problem = prob
         result_actual.fit_status = 'success'
-        result_actual.fit_chi2 = 0.7924313865920499
-        result_actual.params = [-39.66490989383394, 0.001709322146077212, 620.2994253222543,
-                                4.926500627722128, 0.030925377035352437, 24004.503970283724,
-                                13.856560250253684]
-        result_actual.errors = [77.06614570436095, 0.003207694697161955, 109.8358663580242,
-                                204.44335838153586, 0.018928810783550146, 16.39950243454981,
-                                6.285009128709213]
-        result_actual.sum_err_sq = np.sum(np.square(fit_wks_actual.readY(2)))
+        result_actual.fit_chi2 = 1.1185877496775354
+        result_actual.params = [0.3377317528909583, 1.6257361279858433e-05,
+                                711.75402584884307, 0.28815570142467106,
+                                0.029335760313929541, 24007.088835795294,
+                                15.053948033489544]
+        result_actual.errors = [66.243911758365797, 0.0027592920773062742,
+                                105.11921297174581, 0.97460177680550775,
+                                0.015044091663499503, 18.567195196484423,
+                                9.8862122300457997]
+        result_actual.sum_err_sq = 387.9602044
 
+        minimizers = ['Levenberg-Marquardt']
+        use_errors = True
+        count = 0
+        previous_name = "none"
         result = do_fitting_benchmark_one_problem(prob, minimizers, use_errors,
-                                                   count, previous_name)
+                                                  count, previous_name)
         result = result[0][0]
         self.assertEqual(result_actual.problem, result.problem)
         self.assertEqual(result_actual.fit_status, result.fit_status)
@@ -175,24 +168,93 @@ class FittingBenchmarkingOneProblem(unittest.TestCase):
     def test_do_fitting_benchmark_one_problem_nist(self):
 
         prob = self.NISTproblem()
+        results_actual = []
+
+        result1_actual = test_result.FittingTestResult()
+        result1_actual.problem = prob
+        result1_actual.fit_status = 'success'
+        result1_actual.fit_chi2 = 3.0142776470113924e-05
+        result1_actual.params = [234.06483564181511, 0.0005635749331078056]
+        result1_actual.errors = [486.46049489836878, 0.0013377443749239895]
+        result1_actual.sum_err_sq = 0.159784541
+
+        result2_actual = test_result.FittingTestResult()
+        result2_actual.problem = prob
+        result2_actual.fit_status = 'success'
+        result2_actual.fit_chi2 = 3.0142776474075721e-05
+        result2_actual.params = [234.0648164895841, 0.00056357498391696424]
+        result2_actual.errors = [486.46041069760378, 0.0013377443887498521]
+        result2_actual.sum_err_sq = 0.159784814
+
         minimizers = ['Levenberg-Marquardt']
         use_errors = True
         count = 0
         previous_name = "none"
+        results = do_fitting_benchmark_one_problem(prob, minimizers, use_errors,
+                                                   count, previous_name)
+
+        result = results[0][0]
+        self.assertEqual(result1_actual.problem, result.problem)
+        self.assertEqual(result1_actual.fit_status, result.fit_status)
+        self.assertAlmostEqual(result1_actual.fit_chi2, result.fit_chi2)
+        self.assertAlmostEqual(result1_actual.sum_err_sq, result.sum_err_sq, 5)
+        self.assertAlmostEqual(result1_actual.params[0],result.params[0], 3)
+        self.assertAlmostEqual(result1_actual.params[1],result.params[1])
+        self.assertAlmostEqual(result1_actual.errors[0],result.errors[0], 3)
+        self.assertAlmostEqual(result1_actual.errors[1],result.errors[1])
+
+        result = results[1][0]
+        self.assertEqual(result2_actual.problem, result.problem)
+        self.assertEqual(result2_actual.fit_status, result.fit_status)
+        self.assertAlmostEqual(result2_actual.fit_chi2, result.fit_chi2)
+        self.assertAlmostEqual(result2_actual.sum_err_sq, result.sum_err_sq, 5)
+        self.assertAlmostEqual(result2_actual.params[0],result.params[0], 3)
+        self.assertAlmostEqual(result2_actual.params[1],result.params[1])
+        self.assertAlmostEqual(result2_actual.errors[0],result.errors[0], 3)
+        self.assertAlmostEqual(result2_actual.errors[1],result.errors[1])
+
+    def test_do_fitting_benchmark_one_problem_mantidFit_fails(self):
+
+        prob = self.NeutronProblemMock()
+
+        result_actual = test_result.FittingTestResult()
+        result_actual.problem = prob
+        result_actual.fit_status = 'failed'
+        result_actual.fit_chi2 = np.nan
+        result_actual.params = np.nan
+        result_actual.errors = np.nan
+        result_actual.sum_err_sq = np.nan
+
+        minimizers = ['Levenberg-Marquardetss']
+        use_errors = True
+        count = 0
+        previous_name = "none"
+        result = do_fitting_benchmark_one_problem(prob, minimizers, use_errors,
+                                                  count, previous_name)
+
+        result = result[0][0]
+        self.assertEqual(result_actual.problem, result.problem)
+        self.assertEqual(result_actual.fit_status, result.fit_status)
+        self.assertTrue(np.testing.assert_equal(result_actual.fit_chi2, result.fit_chi2) is None)
+        self.assertTrue(np.testing.assert_equal(result_actual.sum_err_sq, result.sum_err_sq) is None)
+        self.assertTrue(np.testing.assert_equal(result_actual.params, result.params) is None)
+        self.assertTrue(np.testing.assert_equal(result_actual.errors, result.errors) is None)
 
 
     def test_run_fit(self):
 
         reference_fit_wks_path = self.NeutronProblemReferenceFitWks()
-        status_actual = 'success'
-        chi2_actual = 0.7924313865920499
         fit_wks_actual = msapi.Load(reference_fit_wks_path)
-        params_actual = [-39.66490989383394, 0.001709322146077212, 620.2994253222543,
-                         4.926500627722128, 0.030925377035352437, 24004.503970283724,
-                         13.856560250253684]
-        errors_actual = [77.06614570436095, 0.003207694697161955, 109.8358663580242,
-                         204.44335838153586, 0.018928810783550146, 16.39950243454981,
-                         6.285009128709213]
+        status_actual = 'success'
+        chi2_actual = 1.1185877496775354
+        params_actual = [0.3377317528909583, 1.6257361279858433e-05,
+                         711.75402584884307, 0.28815570142467106,
+                         0.029335760313929541, 24007.088835795294,
+                         15.053948033489544]
+        errors_actual = [66.243911758365797, 0.0027592920773062742,
+                         105.11921297174581, 0.97460177680550775,
+                         0.015044091663499503, 18.567195196484423,
+                         9.8862122300457997]
 
         prob = self.NeutronProblem()
         wks = msapi.CreateWorkspace(DataX=prob.data_pattern_in,
@@ -204,12 +266,37 @@ class FittingBenchmarkingOneProblem(unittest.TestCase):
         status, chi2, fit_wks, params, errors = run_fit(wks, prob, function,
                                                         minimizer, cost_function)
 
-        self.assertEqual(status,status_actual)
-        self.assertEqual(chi2,chi2_actual)
-        self.assertListEqual(params,params_actual)
-        self.assertListEqual(errors,errors_actual)
-        result, messages = msapi.CompareWorkspaces(fit_wks, fit_wks_actual)
+        self.assertEqual(status_actual,status)
+        self.assertEqual(chi2_actual,chi2)
+        self.assertListEqual(params_actual,params)
+        self.assertListEqual(errors_actual,errors)
+        result, messages = msapi.CompareWorkspaces(fit_wks_actual, fit_wks)
         self.assertTrue(result)
+
+
+    def test_run_fit_mantidFit_fails(self):
+
+        status_actual = 'failed'
+        chi2_actual = np.nan
+        fit_wks_actual = np.nan
+        params_actual = np.nan
+        errors_actual = np.nan
+
+        prob = self.NeutronProblemMock()
+        wks = msapi.CreateWorkspace(DataX=prob.data_pattern_in,
+                                    DataY=prob.data_pattern_out,
+                                    DataE=prob.data_pattern_obs_errors)
+        function = prob.equation
+        minimizer = 'Levenberg-Merquardtss'
+        cost_function = 'Least squared'
+        status, chi2, fit_wks, params, errors = run_fit(wks, prob, function,
+                                                        minimizer, cost_function)
+
+        self.assertEqual(status_actual,status)
+        self.assertTrue(np.testing.assert_equal(chi2_actual,chi2) is None)
+        self.assertTrue(np.testing.assert_equal(params_actual,params) is None)
+        self.assertTrue(np.testing.assert_equal(errors_actual,errors) is None)
+        self.assertTrue(np.testing.assert_equal(fit_wks_actual, fit_wks) is None)
 
 
     def test_prepare_wks_cost_function(self):
@@ -226,10 +313,10 @@ class FittingBenchmarkingOneProblem(unittest.TestCase):
 
         wks, cost_function = prepare_wks_cost_function(prob, use_errors)
 
-        result, messages = msapi.CompareWorkspaces(wks, wks_actual)
+        result, messages = msapi.CompareWorkspaces(wks_actual, wks)
         self.assertTrue(result, msg=("Failed at use_errors = True and "
                                      "obs_errors is a numpy array"))
-        self.assertEqual(cost_function, cost_function_actual,
+        self.assertEqual(cost_function_actual, cost_function,
                          msg=("Failed at use_errors = True and "
                               "obs_errors is a numpy array"))
 
@@ -241,9 +328,9 @@ class FittingBenchmarkingOneProblem(unittest.TestCase):
 
         wks, cost_function = prepare_wks_cost_function(prob, use_errors)
 
-        result, messages = msapi.CompareWorkspaces(wks, wks_actual)
+        result, messages = msapi.CompareWorkspaces(wks_actual, wks)
         self.assertTrue(result, msg="Failed at use_errors = False")
-        self.assertEqual(cost_function, cost_function_actual,
+        self.assertEqual(cost_function_actual, cost_function,
                          msg="Failed at use_errors = False")
 
         # Test when use_errors is True and obs_errors is not a numpy arrray
@@ -258,33 +345,35 @@ class FittingBenchmarkingOneProblem(unittest.TestCase):
 
         wks, cost_function = prepare_wks_cost_function(prob, use_errors)
 
-        result, messages = msapi.CompareWorkspaces(wks, wks_actual)
+        result, messages = msapi.CompareWorkspaces(wks_actual, wks)
         self.assertTrue(result, msg=("Failed at use_errors = True and "
                                      "obs_errors is not a numpy array"))
-        self.assertEqual(cost_function, cost_function_actual,
+        self.assertEqual(cost_function_actual, cost_function,
                          msg=("Failed at use_errors = True and "
                               "obs_errors is not a numpy array"))
 
 
     def test_get_function_definitions(self):
 
+        # Test when prob.starting_values = None
         prob = self.NeutronProblemMock()
-        function_defs_actual = [prob.equation]
 
+        function_defs_actual = [prob.equation]
         function_defs = get_function_definitions(prob)
 
-        self.assertListEqual(function_defs, function_defs_actual,
+        self.assertListEqual(function_defs_actual, function_defs,
                              msg="Failed when prob.starting_values is none")
 
-        prob = self.NISTproblem()
+        # Test when prob.starting_values = to something
         function_defs_actual = ["name=UserFunction, Formula=b1*(1-exp(-b2*x)), "
                                 "b1=500.0,b2=0.0001,",
                                 "name=UserFunction, Formula=b1*(1-exp(-b2*x)), "
                                 "b1=250.0,b2=0.0005," ]
 
+        prob = self.NISTproblem()
         function_defs = get_function_definitions(prob)
 
-        self.assertListEqual(function_defs, function_defs_actual,
+        self.assertListEqual(function_defs_actual, function_defs,
                              msg="Failed when prob.starting_values is something")
 
 
