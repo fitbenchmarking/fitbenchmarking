cdf19273c9cda19fb85fd58ca35e837091e57ef1
Patrick Odagiu <33397154+bb511@users.noreply.github.com>
2018-07-19 13:23:30 +0100

    Cleaned the code and modified how a few things are done

--- fitbenchmarking/test/LoadAndParsingNeutronFileTest.py
+++ fitbenchmarking/test/LoadAndParsingNeutronFileTest.py
@@ -2,6 +2,7 @@ from __future__ import (absolute_import, division, print_function)
 
 import unittest
 import os
+import numpy as np
 
 # DELETE RELATIVE PATH WHEN GIT TESTS ARE ENABLED
 import sys
@@ -14,10 +15,16 @@ from input_parsing import get_neutron_data_problem_entries
 from input_parsing import get_fitting_neutron_data
 import test_problem
 
+# Note for readability: all tests follow the same structure, i.e. :
+# setting up expected results
+# calculating the actual results
+# comparing the two
+# Each of these sections is delimited by an empty new line.
+
 class LoadAndParseNeutronFiles(unittest.TestCase):
 
     def NeutronProblemDirPath(self):
-        ''' Helper function that returns the directory path ../Neutron_/ '''
+        ''' Helper function that returns the directory path ../Neutron_data/ '''
 
         current_dir = os.path.dirname(os.path.realpath(__file__))
         base_dir = os.path.sep.join(current_dir.split(os.path.sep)[:-2])
@@ -27,9 +34,12 @@ class LoadAndParseNeutronFiles(unittest.TestCase):
         return neutron_problems_path
 
     def ENGINX193749Peak19File(self):
+        ''' Helper function that returns the path to the problem file
+            'ENGINX193749_calibration_peak19.txt' '''
+
         neutron_problems_path = self.NeutronProblemDirPath()
         enginxPeak19_path = os.path.join(neutron_problems_path,
-                                        'ENGINX193749_calibration_peak19.txt')
+                                         'ENGINX193749_calibration_peak19.txt')
 
         return enginxPeak19_path
 
@@ -37,67 +47,74 @@ class LoadAndParseNeutronFiles(unittest.TestCase):
     def test_load_neutron_data_fitting_problem_file(self):
 
         enginxPeak19_path = self.ENGINX193749Peak19File()
-        prob_actual = test_problem.FittingTestProblem()
-        prob_actual.name = 'ENGINX 193749 calibration, spectrum 651, peak 19'
-        prob_actual.equation = ("name=LinearBackground;"
-                                "name=BackToBackExponential,"
-                                "I=597.076,A=1,B=0.05,X0=24027.5,S=22.9096")
-        prob_actual.starting_values = None
-        prob_actual.start_x = 23919.5789114
-        prob_actual.end_x = 24189.3183142
-
+        prob_expected = test_problem.FittingTestProblem()
+        prob_expected.name = 'ENGINX 193749 calibration, spectrum 651, peak 19'
+        prob_expected.equation = ("name=LinearBackground;"
+                                  "name=BackToBackExponential,"
+                                  "I=597.076,A=1,B=0.05,X0=24027.5,S=22.9096")
+        prob_expected.starting_values = None
+        prob_expected.start_x = 23919.5789114
+        prob_expected.end_x = 24189.3183142
 
         prob = load_neutron_data_fitting_problem_file(enginxPeak19_path)
 
-        self.assertEqual(prob_actual.name, prob.name,
-                         msg="Test failed at comparing name")
-        self.assertEqual(prob_actual.equation, prob.equation,
-                         msg="Test failed at comparing equation")
-        self.assertEqual(prob_actual.starting_values, prob.starting_values,
-                         msg="Test failed at comparing starting_values")
-        self.assertEqual(prob_actual.start_x, prob.start_x,
-                         msg="Test failed at comparing start_x")
-        self.assertEqual(prob_actual.end_x, prob.end_x,
-                         msg="Test failed at comparing end_x")
+        self.assertEqual(prob_expected.name, prob.name)
+        self.assertEqual(prob_expected.equation, prob.equation)
+        self.assertEqual(prob_expected.starting_values, prob.starting_values)
+        self.assertEqual(prob_expected.start_x, prob.start_x)
+        self.assertEqual(prob_expected.end_x, prob.end_x)
 
 
     def test_get_neutron_data_problem_entries(self):
+
         enginxPeak19_path = self.ENGINX193749Peak19File()
-        entries_actual = {'name' : ("ENGINX 193749 calibration, "
+        entries_expected = {'name' : ("ENGINX 193749 calibration, "
                                     "spectrum 651, peak 19"),
-                          'input_file' : 'ENGINX193749_calibration_spec651.nxs',
-                          'function' : ("name=LinearBackground;"
-                                        "name=BackToBackExponential,"
-                                        "I=597.076,A=1,B=0.05,X0=24027.5,"
-                                        "S=22.9096"),
-                          'fit_parameters' : {'StartX': 23919.5789114,
-                                              'EndX': 24189.3183142},
-                          'description' : ''}
+                            'input_file' : 'ENGINX193749_calibration_spec651.nxs',
+                            'function' : ("name=LinearBackground;"
+                                          "name=BackToBackExponential,"
+                                          "I=597.076,A=1,B=0.05,X0=24027.5,"
+                                          "S=22.9096"),
+                            'fit_parameters' : {'StartX': 23919.5789114,
+                                                'EndX': 24189.3183142},
+                            'description' : ''}
 
         with open(enginxPeak19_path) as probf:
             entries = get_neutron_data_problem_entries(probf)
 
-        self.assertEqual(entries_actual['name'], entries['name'],
-                         msg="Test failed at comparing name")
-        self.assertEqual(entries_actual['input_file'], entries['input_file'],
-                         msg="Test failed at comparing input_file")
-        self.assertEqual(entries_actual['function'], entries['function'],
-                         msg="Test failed at comparing function")
-        self.assertEqual(entries_actual['fit_parameters'], entries['fit_parameters'],
-                         msg="Test failed at comparing fit_parameters")
-        self.assertEqual(entries_actual['description'], entries['description'],
-                         msg="Test failed at comparing description")
+        self.assertEqual(entries_expected['name'], entries['name'])
+        self.assertEqual(entries_expected['input_file'], entries['input_file'])
+        self.assertEqual(entries_expected['function'], entries['function'])
+        self.assertEqual(entries_expected['fit_parameters'], entries['fit_parameters'])
+        self.assertEqual(entries_expected['description'], entries['description'])
+
+
+    def MockProblemData(self):
+        ''' Helper function that provides the data in NeutronMockData.nxs
+            stored in a test_problem object '''
+
+        prob = test_problem.FittingTestProblem()
+
+        prob.data_pattern_in = np.array([1, 2, 3, 4, 5, 6])
+        prob.data_pattern_out = np.array([7, 8, 9, 10, 11, 12])
+        prob.data_pattern_obs_errors = np.sqrt(prob.data_pattern_out)
+
+        return prob
 
 
     def test_get_fitting_neutron_data(self):
-        # QUESTION: HOW DO I TEST THIS?
 
-        neutron_problems_path = self.NeutronProblemDirPath()
-        neutron_fit_file = os.path.join(neutron_problems_path, 'data_files',
-                                        'ENGINX193749_calibration_spec651.nxs')
+        prob_expected = self.MockProblemData()
+
+        current_dir = os.path.dirname(os.path.realpath(__file__))
+        neutron_mock_data = os.path.join(current_dir, 'mock_problems', 'data_files',
+                                         'NeutronMockData.nxs')
         prob = test_problem.FittingTestProblem()
+        get_fitting_neutron_data(neutron_mock_data, prob)
 
-        get_fitting_neutron_data(neutron_fit_file, prob)
+        np.testing.assert_array_equal(prob_expected.data_pattern_in, prob.data_pattern_in)
+        np.testing.assert_array_equal(prob_expected.data_pattern_out, prob.data_pattern_out)
+        np.testing.assert_array_equal(prob_expected.data_pattern_obs_errors, prob.data_pattern_obs_errors)
 
 
 if __name__ == "__main__":
--- fitbenchmarking/test/LoadAndParsingNistFileTest.py
+++ fitbenchmarking/test/LoadAndParsingNistFileTest.py
@@ -18,6 +18,12 @@ from input_parsing import parse_equation
 from input_parsing import parse_starting_values
 import test_problem
 
+# Note for readability: all tests follow the same structure, i.e. :
+# setting up expected results
+# calculating the actual results
+# comparing the two
+# Each of these sections is delimited by an empty new line.
+
 class LoadAndParseNistFiles(unittest.TestCase):
 
     def nistProblemDirPath(self):
@@ -31,6 +37,7 @@ class LoadAndParseNistFiles(unittest.TestCase):
 
         return nist_problems_path
 
+
     def Misra1aLines(self):
         ''' Helper function that returns the lines of problem file Misra1a.dat '''
 
@@ -41,6 +48,7 @@ class LoadAndParseNistFiles(unittest.TestCase):
     	   lines = spec_file.readlines()
 
         return lines
+
 
     def Bennett5Lines(self):
         ''' Helper function that returns the lines of problem file Bennett5.dat '''
@@ -55,209 +63,169 @@ class LoadAndParseNistFiles(unittest.TestCase):
 
 
     def ResultsMisra1a(self):
-    	''' What parsing the Misra1a.dat file should return '''
+    	''' Expected results from parsing the Misra1a.dat file '''
 
+    	equation_text = 'y = b1*(1-exp[-b2*x])  +  e'
+    	starting_values = [['b1', [500.0,250.0]],
+    					   ['b2', [0.0001,0.0005]]]
+    	residual_sum_sq = 1.2455138894E-01
+    	data_pattern_text = ['      10.07E0      77.6E0\n',
+    						 '      14.73E0     114.9E0\n',
+							 '      17.94E0     141.1E0\n',
+							 '      23.93E0     190.8E0\n',
+							 '      29.61E0     239.9E0\n',
+							 '      35.18E0     289.0E0\n',
+							 '      40.02E0     332.8E0\n',
+							 '      44.82E0     378.4E0\n',
+							 '      50.76E0     434.8E0\n',
+							 '      55.05E0     477.3E0\n',
+							 '      61.01E0     536.8E0\n',
+							 '      66.40E0     593.1E0\n',
+							 '      75.47E0     689.1E0\n',
+							 '      81.78E0     760.0E0\n' ]
 
-    	equation_text_actual = 'y = b1*(1-exp[-b2*x])  +  e'
-    	starting_values_actual = [['b1', [500.0,250.0]],
-    							  ['b2', [0.0001,0.0005]]]
-    	residual_sum_sq_actual = 1.2455138894E-01
-    	data_pattern_text_actual = ['      10.07E0      77.6E0\n',
-    								'      14.73E0     114.9E0\n',
-									'      17.94E0     141.1E0\n',
-									'      23.93E0     190.8E0\n',
-									'      29.61E0     239.9E0\n',
-									'      35.18E0     289.0E0\n',
-									'      40.02E0     332.8E0\n',
-									'      44.82E0     378.4E0\n',
-									'      50.76E0     434.8E0\n',
-									'      55.05E0     477.3E0\n',
-									'      61.01E0     536.8E0\n',
-									'      66.40E0     593.1E0\n',
-									'      75.47E0     689.1E0\n',
-									'      81.78E0     760.0E0\n' ]
-
-        return (equation_text_actual, starting_values_actual,
-                residual_sum_sq_actual, data_pattern_text_actual)
+        return (equation_text, starting_values,
+                residual_sum_sq, data_pattern_text)
 
 
     def test_parse_nist_file_line_by_line(self):
 
+    	(equation_text_expected, starting_values_expected,
+    	 residual_sum_sq_expected, data_pattern_text_expected) = self.ResultsMisra1a()
+
         lines = self.Misra1aLines()
+        (equation_text, data_pattern_text,
+         starting_values, residual_sum_sq) = parse_nist_file_line_by_line(lines)
 
-    	(equation_text, data_pattern_text,
-    	 starting_values, residual_sum_sq) = parse_nist_file_line_by_line(lines)
-
-    	(equation_text_actual, starting_values_actual,
-    	 residual_sum_sq_actual, data_pattern_text_actual) = self.ResultsMisra1a()
-
-    	self.assertEqual(equation_text, equation_text_actual,
-                         msg="Test failed when comparing equations")
-    	self.assertItemsEqual(data_pattern_text, data_pattern_text_actual,
-    						  msg="Test failed comparing data pattern length")
-    	self.assertListEqual(data_pattern_text, data_pattern_text_actual,
-    						 msg="Test failed comparing data pattern content")
-    	self.assertEqual(residual_sum_sq, residual_sum_sq_actual,
-    					 msg="Test failed when comparing residual sums")
-    	for idx in range(0, len(starting_values_actual)):
-    		self.assertEqual(starting_values_actual[idx][0],
-    						 starting_values[idx][0],
-    						 msg="Test failed at starting values")
-    		self.assertListEqual(starting_values_actual[idx][1],
-    			                 starting_values_actual[idx][1],
-    			                 msg="Test failed at starting values")
+    	self.assertEqual(equation_text_expected, equation_text)
+    	self.assertItemsEqual(data_pattern_text_expected, data_pattern_text)
+    	self.assertListEqual(data_pattern_text_expected, data_pattern_text)
+    	self.assertEqual(residual_sum_sq_expected, residual_sum_sq)
+    	for idx in range(0, len(starting_values_expected)):
+    		self.assertEqual(starting_values_expected[idx][0], starting_values[idx][0])
+    		self.assertListEqual(starting_values_expected[idx][1], starting_values[idx][1])
 
 
-    def test_parse_nist_file_line_by_line_fail_wrong_file(self):
+    def test_fail_wrong_file_parse_nist_file_line_by_line(self):
 
-    	lines = self.Bennett5Lines()
-    	(equation_text, data_pattern_text,
-    	 starting_values, residual_sum_sq) = parse_nist_file_line_by_line(lines)
+    	(equation_text_expected, starting_values_expected,
+    	 residual_sum_sq_expected, data_pattern_text_expected) = self.ResultsMisra1a()
 
-    	(equation_text_actual, starting_values_actual,
-    	 residual_sum_sq_actual, data_pattern_text_actual) = self.ResultsMisra1a()
+        lines = self.Bennett5Lines()
+        (equation_text, data_pattern_text,
+         starting_values, residual_sum_sq) = parse_nist_file_line_by_line(lines)
 
-    	self.assertEqual(equation_text, equation_text_actual,
-    					 msg="Test failed when comparing equations")
-    	self.assertItemsEqual(data_pattern_text, data_pattern_text_actual,
-    						  msg="Test failed comparing data pattern length")
-    	self.assertListEqual(data_pattern_text, data_pattern_text_actual,
-    						 msg="Test failed comparing data pattern content")
-    	self.assertEqual(residual_sum_sq, residual_sum_sq_actual,
-    					 msg="Test failed when comparing residual sums")
-    	for idx in range(0, len(starting_values_actual)):
-    		self.assertEqual(starting_values_actual[idx][0],
-    						 starting_values[idx][0],
-    						 msg="Test failed at starting values")
-    		self.assertListEqual(starting_values_actual[idx][1],
-    			                 starting_values_actual[idx][1],
-    			                 msg="Test failed at starting values")
+    	self.assertEqual(equation_text_expected, equation_text)
+    	self.assertItemsEqual(data_pattern_text_expected, data_pattern_text)
+    	self.assertListEqual(data_pattern_text_expected, data_pattern_text)
+    	self.assertEqual(residual_sum_sq_expected, residual_sum_sq)
+    	for idx in range(0, len(starting_values_expected)):
+    		self.assertEqual(starting_values_expected[idx][0], starting_values[idx][0])
+    		self.assertListEqual(starting_values_expected[idx][1], starting_values[idx][1])
 
 
     def test_parse_starting_values(self):
 
-        idx = 0
-        lines = self.Misra1aLines()
-        while idx < len(lines):
-            line = lines[idx].strip()
-            idx += 1
+        starting_values_expected = [['b1', [500.0,250.0]],
+                                    ['b2', [0.0001,0.0005]]]
 
-            if not line:
-                continue
+        lines = ["  b1 =   500         250           2.3894212918E+02  2.7070075241E+00",
+                 "  b2 =     0.0001      0.0005      5.5015643181E-04  7.2668688436E-06" ]
+        starting_values = parse_starting_values(lines)
 
-            if 'Starting values' in line or 'Starting Values' in line:
-                idx += 2
-                starting_values = parse_starting_values(lines[idx:])
-
-        starting_values_actual = [['b1', [500.0,250.0]],
-                                  ['b2', [0.0001,0.0005]]]
-
-        for idx in range(0, len(starting_values_actual)):
-            self.assertEqual(starting_values_actual[idx][0],
-                             starting_values[idx][0])
-            self.assertListEqual(starting_values_actual[idx][1],
-                                 starting_values_actual[idx][1])
+        print(starting_values)
+        for idx in range(0, len(starting_values_expected)):
+            self.assertEqual(starting_values_expected[idx][0], starting_values[idx][0])
+            self.assertListEqual(starting_values_expected[idx][1], starting_values[idx][1])
 
 
     def test_parse_data_pattern(self):
 
-        lines = self.Misra1aLines()
+        data_pattern_actual = np.array([ [10.07, 77.6],
+                                         [14.73, 114.9],
+                                         [17.94, 141.1],
+                                         [23.93, 190.8],
+                                         [29.61, 239.9],
+                                         [35.18, 289.0],
+                                         [40.02, 332.8],
+                                         [44.82, 378.4],
+                                         [50.76, 434.8],
+                                         [55.05, 477.3],
+                                         [61.01, 536.8],
+                                         [66.40, 593.1],
+                                         [75.47, 689.1],
+                                         [81.78, 760.0] ])
 
-        (equation_text, data_pattern_text,
-         starting_values, residual_sum_sq) = parse_nist_file_line_by_line(lines)
-
+        data_pattern_text = ['      10.07E0      77.6E0\n',
+                             '      14.73E0     114.9E0\n',
+                             '      17.94E0     141.1E0\n',
+                             '      23.93E0     190.8E0\n',
+                             '      29.61E0     239.9E0\n',
+                             '      35.18E0     289.0E0\n',
+                             '      40.02E0     332.8E0\n',
+                             '      44.82E0     378.4E0\n',
+                             '      50.76E0     434.8E0\n',
+                             '      55.05E0     477.3E0\n',
+                             '      61.01E0     536.8E0\n',
+                             '      66.40E0     593.1E0\n',
+                             '      75.47E0     689.1E0\n',
+                             '      81.78E0     760.0E0\n' ]
         data_pattern = parse_data_pattern(data_pattern_text)
-        data_pattern_actual = [ [10.07, 77.6],
-                                [14.73, 114.9],
-                                [17.94, 141.1],
-                                [23.93, 190.8],
-                                [29.61, 239.9],
-                                [35.18, 289.0],
-                                [40.02, 332.8],
-                                [44.82, 378.4],
-                                [50.76, 434.8],
-                                [55.05, 477.3],
-                                [61.01, 536.8],
-                                [66.40, 593.1],
-                                [75.47, 689.1],
-                                [81.78, 760.0] ]
 
-        data_pattern_actual = np.asarray(data_pattern_actual)
-
-        for i in range(0,len(data_pattern_actual)):
-            test_result = np.testing.assert_array_equal(data_pattern_actual[i],
-                                                        data_pattern[i])
-            self.assertTrue(test_result is None)
+        for idx in range(0,len(data_pattern_actual)):
+            np.testing.assert_array_equal(data_pattern_actual[idx], data_pattern[idx])
 
 
     def test_parse_equation(self):
 
-        lines = self.Misra1aLines()
-        (equation_text, data_pattern_text,
-         starting_values, residual_sum_sq) = parse_nist_file_line_by_line(lines)
+        parsed_eq_expected = 'b1*(1-exp(-b2*x))'
 
+        equation_text = 'y = b1*(1-exp[-b2*x])  +  e'
         parsed_eq = parse_equation(equation_text)
-        parsed_eq_actual = 'b1*(1-exp(-b2*x))'
 
-        self.assertEqual(parsed_eq, parsed_eq_actual,
-                         msg="Test failed at parsing equations normal")
-
+        self.assertEqual(parsed_eq_expected, parsed_eq)
         self.assertRaises(RuntimeError, parse_equation, 'lol = b1*(1-exp(-b2*x))')
-
 
 
     def test_parse_nist_file(self):
 
-        data_pattern_actual = [ [10.07, 77.6],
-                                [14.73, 114.9],
-                                [17.94, 141.1],
-                                [23.93, 190.8],
-                                [29.61, 239.9],
-                                [35.18, 289.0],
-                                [40.02, 332.8],
-                                [44.82, 378.4],
-                                [50.76, 434.8],
-                                [55.05, 477.3],
-                                [61.01, 536.8],
-                                [66.40, 593.1],
-                                [75.47, 689.1],
-                                [81.78, 760.0] ]
-        data_pattern_actual = np.asarray(data_pattern_actual)
-
-        prob_actual = test_problem.FittingTestProblem()
-        prob_actual.name = 'Misra1a.dat'
-        prob_actual.linked_name = ("`Misra1a.dat <http://www.itl.nist.gov/"
+        data_pattern_expected = np.array([ [10.07, 77.6],
+                                           [14.73, 114.9],
+                                           [17.94, 141.1],
+                                           [23.93, 190.8],
+                                           [29.61, 239.9],
+                                           [35.18, 289.0],
+                                           [40.02, 332.8],
+                                           [44.82, 378.4],
+                                           [50.76, 434.8],
+                                           [55.05, 477.3],
+                                           [61.01, 536.8],
+                                           [66.40, 593.1],
+                                           [75.47, 689.1],
+                                           [81.78, 760.0] ])
+        prob_expected = test_problem.FittingTestProblem()
+        prob_expected.name = 'Misra1a.dat'
+        prob_expected.linked_name = ("`Misra1a.dat <http://www.itl.nist.gov/"
                                    "div898/strd/nls/data/misra1a.dat.shtml>`__")
-        prob_actual.equation = 'b1*(1-exp(-b2*x))'
-        prob_actual.starting_values = [['b1', [500.0,250.0]],
+        prob_expected.equation = 'b1*(1-exp(-b2*x))'
+        prob_expected.starting_values = [['b1', [500.0,250.0]],
                                        ['b2', [0.0001,0.0005]]]
-        prob_actual.data_pattern_in = data_pattern_actual[:, 1:]
-        prob_actual.data_pattern_out = data_pattern_actual[:, 0]
-        prob_actual.ref_residual_sum_sq = 1.2455138894E-01
+        prob_expected.data_pattern_in = data_pattern_expected[:, 1:]
+        prob_expected.data_pattern_out = data_pattern_expected[:, 0]
+        prob_expected.ref_residual_sum_sq = 1.2455138894E-01
 
         base_path = self.nistProblemDirPath()
         path_to_misra1a = os.path.join(base_path,'Misra1a.dat')
         with open(path_to_misra1a) as spec_file:
             prob = parse_nist_file(spec_file)
 
-        self.assertEqual(prob.name, prob_actual.name,
-                         msg="Fail at name")
-        self.assertEqual(prob.linked_name, prob_actual.linked_name,
-                         msg="Fail at linked name")
-        self.assertEqual(prob.equation, prob_actual.equation,
-                         msg="Fail at equation")
-        self.assertEqual(prob.starting_values, prob_actual.starting_values,
-                         msg="Fail at starting values")
-        test_result = np.testing.assert_array_equal(prob_actual.data_pattern_in,
-                                                        prob.data_pattern_in)
-        self.assertTrue(test_result is None, msg="Fail at data pattern in")
-
-        test_result = np.testing.assert_array_equal(prob_actual.data_pattern_out,
-                                                        prob.data_pattern_out)
-        self.assertTrue(test_result is None, msg="Fail at data pattern out")
-
-        self.assertEqual(prob.ref_residual_sum_sq, prob_actual.ref_residual_sum_sq,
-                         msg="Fail at ref residual sum sq")
+        self.assertEqual(prob_expected.name, prob.name)
+        self.assertEqual(prob_expected.linked_name, prob.linked_name)
+        self.assertEqual(prob_expected.equation, prob.equation)
+        self.assertEqual(prob_expected.starting_values, prob.starting_values)
+        self.assertEqual(prob_expected.ref_residual_sum_sq, prob.ref_residual_sum_sq)
+        np.testing.assert_array_equal(prob_expected.data_pattern_in, prob.data_pattern_in)
+        np.testing.assert_array_equal(prob_expected.data_pattern_out, prob.data_pattern_out)
 
 
 if __name__ == "__main__":
