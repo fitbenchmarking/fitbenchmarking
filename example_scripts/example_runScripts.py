59619ec7b5db959483e947244e405bae43c576f3
Patrick <patrick.odagiu>
2018-07-31 15:43:42 +0100

    refs #48 Cleaned example_runScripts.py

--- example_scripts/example_runScripts.py
+++ example_scripts/example_runScripts.py
@@ -1,12 +1,36 @@
-# script for running fit benchmarking and comparising the relative performance of local minimzers on
-# fit problems
+"""
+Script that runs the fitbenchmarking tool with various problems and minimizers.
+"""
+# Copyright &copy; 2016 ISIS Rutherford Appleton Laboratory, NScD
+# Oak Ridge National Laboratory & European Spallation Source
+#
+# This file is part of Mantid.
+# Mantid is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Mantid is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# File change history is stored at: <https://github.com/mantidproject/mantid>.
+# Code Documentation is available at: <http://doxygen.mantidproject.org>
+# ==============================================================================
+
 from __future__ import (absolute_import, division, print_function)
 
-# sys recursion limit 10000 is needed to avoid reaching the maximum recursion
-# depth when running all the problems (this produces a RuntimeError)
 import os
 import sys
+# Avoid reaching the maximum recursion depth by setting recursion limit
 sys.setrecursionlimit(10000)
+
+# Insert path to where the scripts are located, relative to
+# the example_scripts folder
 current_path = os.path.dirname(os.path.realpath(__file__))
 fitbenchmarking_path = os.path.abspath(os.path.join(current_path, os.pardir))
 scripts_path = os.path.join(fitbenchmarking_path, 'fitbenchmarking')
@@ -15,6 +39,7 @@ sys.path.insert(0, scripts_path)
 from fitting_benchmarking import do_fitting_benchmark as fitBenchmarking
 from results_output import print_group_results_tables as printTables
 
+
 minimizers = ['BFGS', 'Conjugate gradient (Fletcher-Reeves imp.)',
               'Conjugate gradient (Polak-Ribiere imp.)',
               'Damped GaussNewton',
@@ -22,7 +47,8 @@ minimizers = ['BFGS', 'Conjugate gradient (Fletcher-Reeves imp.)',
               'Simplex','SteepestDescent',
               'Trust Region']
 
-group_suffix_names = ['nist_lower', 'nist_average', 'nist_higher', 'cutest', 'neutron_data']
+# Parameters of how the final tables are colored
+# e.g. lower that 1.1 -> light yellow, higher than 3 -> dark red
 color_scale = [(1.1, 'ranking-top-1'),
                (1.33, 'ranking-top-2'),
                (1.75, 'ranking-med-3'),
@@ -30,12 +56,12 @@ color_scale = [(1.1, 'ranking-top-1'),
                (float('nan'), 'ranking-low-5')]
 
 
-base_problem_files_dir = os.path.join(fitbenchmarking_path, 'benchmark_problems')
+benchmark_problems_dir = os.path.join(fitbenchmarking_path, 'benchmark_problems')
 
-nist_group_dir = os.path.join(base_problem_files_dir, 'NIST_nonlinear_regression')
-cutest_group_dir = os.path.join(base_problem_files_dir, 'CUTEst')
-neutron_data_group_dirs = [os.path.join(base_problem_files_dir, 'Neutron_data')]
-muon_data_group_dir = [os.path.join(base_problem_files_dir, 'Muon_data')]
+nist_group_dir = os.path.join(benchmark_problems_dir, 'NIST_nonlinear_regression')
+cutest_group_dir = os.path.join(benchmark_problems_dir, 'CUTEst')
+neutron_data_group_dirs = [os.path.join(benchmark_problems_dir, 'Neutron_data')]
+muon_data_group_dir = [os.path.join(benchmark_problems_dir, 'Muon_data')]
 
 
 # Modify results_dir to specify where the results of the fit should be saved
@@ -44,8 +70,6 @@ muon_data_group_dir = [os.path.join(base_problem_files_dir, 'Muon_data')]
 # If the full path is not given and the results_dir name is valid
 #  ../fitbenchmarking/fitbenchmarking/ is taken as the path
 results_dir = None
-
-# Whether to use errors in the fitting process
 use_errors = True
 
 for run_data in ["neutron", "nist"]:
