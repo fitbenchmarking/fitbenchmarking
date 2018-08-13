4594673dcc9dd61ca87cc91cf5bd3ef1c540d75e
Patrick Odagiu <33397154+bb511@users.noreply.github.com>
2018-08-13 17:20:48 +0100

    refs #91 Added some unit tests for the misc modules and inits for the other testing packages.

---
+++ fitbenchmarking/utils/tests/fit_misc_tests.py
@@ -0,0 +1,29 @@
+from __future__ import (absolute_import, division, print_function)
+
+import unittest
+import os
+
+# Delete four lines below when automated tests ar enabled
+import sys
+test_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(os.path.normpath(test_dir))
+main_dir = os.path.dirname(os.path.normpath(parent_dir))
+sys.path.insert(0, main_dir)
+
+from utils.fit_misc import compute_chisq
+
+
+class FitMiscTests(unittest.TestCase):
+
+    def test_compute_chisq(self):
+
+        differences = np.array([1,2,3])
+
+        chi_sq = compute_chisq(differences)
+        chi_sq_expected = 14
+
+        self.assertEqual(chi_sq_expected, chi_sq)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ fitbenchmarking/utils/tests/setup_problem_groups_tests.py
@@ -0,0 +1,21 @@
+from __future__ import (absolute_import, division, print_function)
+
+import unittest
+import os
+
+# Delete four lines below when automated tests ar enabled
+import sys
+test_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(os.path.normpath(test_dir))
+main_dir = os.path.dirname(os.path.normpath(parent_dir))
+sys.path.insert(0, main_dir)
+
+class SetupProblemGroupsTests(unittest.TestCase):
+
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
