4fe209f61b8c2a2217a784344d843d8effda0dec
Patrick Odagiu <33397154+bb511@users.noreply.github.com>
2018-08-13 10:31:44 +0100

    refs #91 Setting up the parsing problems.

---
+++ fitbenchmarking/parsing/tests/parse_neutron_tests.py
@@ -0,0 +1,20 @@
+from __future__ import (absolute_import, division, print_function)
+
+import unittest
+import os
+import numpy as np
+import mantid.simpleapi as msapi
+
+# Delete four lines below when automated tests ar enabled
+import sys
+test_dir = os.path.dirname(os.path.realpath(__file__))
+parent_dir = os.path.dirname(os.path.normpath(test_dir))
+main_dir = os.path.dirname(os.path.normpath(parent_dir))
+sys.path.insert(0, main_dir)
+
+class Parsing(unittest.TestCase):
+
+
+
+if __name__ == "__main__":
+    unittest.main()
