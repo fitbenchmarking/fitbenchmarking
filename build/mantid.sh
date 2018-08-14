2ddaa2bd575bb50535a760f5d2fbc69ec5ee056f
Patrick Odagiu <33397154+bb511@users.noreply.github.com>
2018-08-14 17:22:38 +0100

    refs #86 Installing mantid dependency.

---
+++ build/mantid.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Test sudo
+$(sudo -n echo)
+exitCode=$?
+if [ $exitCode -ne 0 ] ; then
+    echo "This script must be run with sudo" >&2
+    exit 1
+fi
+
+set -e
+
+# Add GPG key and upstream Mantid repo
+sudo apt-add-repository "deb [arch=amd64] http://apt.isis.rl.ac.uk $(lsb_release -c | cut -f 2) main" -y
+# add the signing key
+wget -O - http://apt.isis.rl.ac.uk/2E10C193726B7213.asc -q | sudo apt-key add -
+sudo apt-add-repository ppa:mantid/mantid -y
+
+sudo apt-get install mantid -y
