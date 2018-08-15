39835757f65033445d8e3d35d720ca11ea03f0ae
Patrick Odagiu <33397154+bb511@users.noreply.github.com>
2018-08-15 15:39:53 +0100

    refs #86 Changing the shell script.

--- build/install/mantid.sh
+++ build/install/mantid.sh
@@ -16,4 +16,6 @@ sudo apt-add-repository "deb [arch=amd64] http://apt.isis.rl.ac.uk $(lsb_release
 # add the signing key
 wget -O - http://apt.isis.rl.ac.uk/2E10C193726B7213.asc -q | sudo apt-key add -
 sudo apt-add-repository ppa:mantid/mantid -y
+
+sudo apt-get update
 sudo apt-get install mantid -y
