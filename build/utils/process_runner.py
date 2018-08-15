d11f3500dad4b5f71758b7a01bf0a3ea403817ac
Patrick Odagiu <33397154+bb511@users.noreply.github.com>
2018-08-15 14:26:02 +0100

    refs #86 Fixed some minor issues.

--- build/utils/process_runner.py
+++ build/utils/process_runner.py
@@ -13,7 +13,6 @@ def run_process_and_log(list_of_args):
 
     @param list_of_args :: list of arguments for Popen
     """
-
     process = subprocess.Popen(list_of_args,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
