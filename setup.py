e582316ff508a9d0a2be76f683089ec1e621d29f
Patrick Odagiu <33397154+bb511@users.noreply.github.com>
2018-08-24 09:09:48 +0100

    refs #86 Added 2 at the end of version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from build.commands.help import Help
 
 
 setup(name='FitBenchmarking',
-      version='0.1dev',
+      version='0.1dev2',
       description='Fit benchmarking software',
       author='ISIS Fit Benchmarking Team',
       url='http://github.com/mantidproject/fitbenchmarking',
