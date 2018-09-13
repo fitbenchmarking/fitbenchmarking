ca6a40b2e86ba210dce4dcb4d4f6cff841043e65
Patrick <patrickodagiu@gmail.com>
2018-09-13 18:35:46 +0100

    refs #117 Making small change

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='FitBenchmarking',
       url='http://github.com/mantidproject/fitbenchmarking',
       license='GPL-3.0',
       packages=find_packages(),
-      install_requires=['docutils'],
+      install_requires=['docutils', 'scipy>=0.17'],
       zip_safe=False,
       cmdclass={
           'externals': InstallExternals,
