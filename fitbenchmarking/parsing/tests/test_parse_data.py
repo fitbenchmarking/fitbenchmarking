b56c34307d332ec6def31e0c24ed90f4a2a21337
Michael Wathen <michael.wathen@stfc.ac.uk>
2019-03-08 14:22:00 +0000

    Updating test files for: parsing and utils

--- fitbenchmarking/parsing/tests/test_parse_data.py
+++ fitbenchmarking/parsing/tests/test_parse_data.py
@@ -159,8 +159,7 @@ class ParseNistTests(unittest.TestCase):
         main_dir = os.path.dirname(os.path.normpath(parent_dir))
         root_dir = os.path.dirname(os.path.normpath(main_dir))
         bench_prob_dir = os.path.join(root_dir, 'benchmark_problems')
-        fname = os.path.join(bench_prob_dir, 'NIST_nonlinear_regression',
-                             'low_diff', 'Misra1a.dat')
+        fname = os.path.join(bench_prob_dir, 'NIST_low_diff', 'Misra1a.dat')
 
         return fname
 
