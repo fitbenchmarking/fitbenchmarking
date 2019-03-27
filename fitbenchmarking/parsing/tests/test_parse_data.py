9c2c49cbd8d78d7e4e07d45e2e61f55a90769eaa
Michael Wathen <michael.wathen@stfc.ac.uk>
2019-03-27 08:54:57 +0000

    Remove spelling mistakes from parsing/tests/test_parse_data.py

--- fitbenchmarking/parsing/tests/test_parse_data.py
+++ fitbenchmarking/parsing/tests/test_parse_data.py
@@ -4,7 +4,7 @@ import unittest
 import os
 import numpy as np
 
-# Delete four lines below when automated tests ar enabled
+# Delete four lines below when automated tests are enabled
 import sys
 test_dir = os.path.dirname(os.path.realpath(__file__))
 parent_dir = os.path.dirname(os.path.normpath(test_dir))
