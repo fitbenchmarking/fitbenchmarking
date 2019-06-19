943f87e440c4c1932957b2430874b16757b6c657
Michael Wathen <mwathen@gmail.com>
2019-06-19 13:37:45 +0100

    Removing fit_parameter bug in EVS14188 data

--- example_scripts/example_runScripts.py
+++ example_scripts/example_runScripts.py
@@ -44,7 +44,7 @@ from fitting_benchmarking import do_fitting_benchmark as fitBenchmarking
 from results_output import save_results_tables as printTables
 
 # SPECIFY THE SOFTWARE/PACKAGE CONTAINING THE MINIMIZERS YOU WANT TO BENCHMARK
-software = ['mantid', 'scipy']
+software = 'mantid'
 software_options = {'software': software}
 
 # User defined minimizers
@@ -93,7 +93,7 @@ color_scale = [(1.1, 'ranking-top-1'),
 # Do this, in this example file, by selecting sub-folders in benchmark_probs_dir
 # "Muon_data" works for mantid minimizers
 # problem_sets = ["Neutron_data", "NIST/average_difficulty"]
-problem_sets = ["NIST/average_difficulty"]
+problem_sets = ["CUTEst", "Muon_data", "Neutron_data", "NIST/average_difficulty", "NIST/high_difficulty", "NIST/low_difficulty"]
 for sub_dir in problem_sets:
   # generate group label/name used for problem set
   label = sub_dir.replace('/', '_')
