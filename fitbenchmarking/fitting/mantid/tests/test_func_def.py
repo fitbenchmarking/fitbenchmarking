61f701d21ef3879a1f6219dd39cf88526a48e91f
Andrew McCluskey <andrew.mccluskey@diamond.ac.uk>
2019-09-19 11:23:24 +0100

    rebase to upstream master

--- fitbenchmarking/fitting/mantid/tests/test_func_def.py
+++ fitbenchmarking/fitting/mantid/tests/test_func_def.py
@@ -1,3 +1,6 @@
+"""
+Mantid function definition testing
+"""
 from __future__ import (absolute_import, division, print_function)
 
 import unittest
@@ -10,93 +13,20 @@ from fitbenchmarking.fitting.mantid.func_def import (
 from fitbenchmarking.fitting.mantid.func_def import (
     parse_function_definitions
 )
-
-from fitbenchmarking.parsing.parse_nist_data import (
-    FittingProblem as NISTFittingProblem
-)
-from fitbenchmarking.parsing.parse_fitbenchmark_data import (
-    FittingProblem as FBFittingProblem
-)
+from fitbenchmarking.parsing.parse_fitbenchmark_data import FittingProblem
 from fitbenchmarking.mock_problem_files.get_problem_files import get_file
+from fitbenchmarking.fitting.mantid.tests.test_main import nist_problem
 
 
 class MantidTests(unittest.TestCase):
-    def NIST_problem(self):
+    """
+    Implements Mantid Function Definition Tests
+    """
+    def test_function_definitions_return_nist_functions(self):
         """
-        Helper function.
-        Sets up the problem object for the nist problem file Misra1a.dat
+        Test the NIST function definitions return the correct functions
         """
-
-        data_pattern = np.array([[10.07, 77.6],
-                                 [14.73, 114.9],
-                                 [17.94, 141.1],
-                                 [23.93, 190.8],
-                                 [29.61, 239.9],
-                                 [35.18, 289.0],
-                                 [40.02, 332.8],
-                                 [44.82, 378.4],
-                                 [50.76, 434.8],
-                                 [55.05, 477.3],
-                                 [61.01, 536.8],
-                                 [66.40, 593.1],
-                                 [75.47, 689.1],
-                                 [81.78, 760.0]])
-
-        fname = get_file('NIST_Misra1a.dat')
-        prob = NISTFittingProblem(fname)
-        prob.name = 'Misra1a'
-        prob.equation = 'b1*(1-exp(-b2*x))'
-        prob.starting_values = [['b1', [500.0, 250.0]],
-                                ['b2', [0.0001, 0.0005]]]
-        prob.data_x = data_pattern[:, 1]
-        prob.data_y = data_pattern[:, 0]
-
-        return prob
-
-    def Neutron_problem(self):
-        """
-        Sets up the problem object for the neutron problem file:
-        ENGINX193749_calibration_peak19.txt
-        """
-
-        fname = get_file('FB_ENGINX193749_calibration_peak19.txt')
-        prob = FBFittingProblem(fname)
-        prob.name = 'ENGINX 193749 calibration, spectrum 651, peak 19'
-        prob.equation = ("name=LinearBackground,A0=0,A1=0;"
-                         "name=BackToBackExponential,"
-                         "I=597.076,A=1,B=0.05,X0=24027.5,S=22.9096")
-        prob.starting_values = None
-        prob.start_x = 23919.5789114
-        prob.end_x = 24189.3183142
-
-        return prob
-
-    def create_wks_NIST_problem_with_errors(self):
-        """
-        Helper function.
-        Creates a mantid workspace using the data provided by the
-        NIST problem Misra1a.
-        """
-
-        prob = self.NIST_problem()
-        data_e = np.sqrt(abs(prob.data_y))
-        wks_exp = msapi.CreateWorkspace(DataX=prob.data_x, DataY=prob.data_y,
-                                        DataE=data_e)
-        return wks_exp
-
-    def create_wks_NIST_problem_without_errors(self):
-        """
-        Helper function.
-        Creates a mantid workspace using the data provided by the
-        NIST problem Misra1a.
-        """
-
-        prob = self.NIST_problem()
-        wks_exp = msapi.CreateWorkspace(DataX=prob.data_x, DataY=prob.data_y)
-        return wks_exp
-
-    def test_functionDefinitions_return_NIST_functions(self):
-        prob = self.NIST_problem()
+        prob = nist_problem()
 
         function_defs = function_definitions(prob)
         function_defs_expected = \
@@ -105,8 +35,11 @@ class MantidTests(unittest.TestCase):
 
         self.assertListEqual(function_defs_expected, function_defs)
 
-    def test_functionDefinitions_return_neutron_function(self):
-        prob = self.Neutron_problem()
+    def test_function_definitions_return_neutron_function(self):
+        """
+        Test the NIST function definitions return the correct function
+        """
+        prob = neutron_problem()
 
         function_defs = function_definitions(prob)
         function_defs = [str(function_defs[0])]
@@ -116,8 +49,11 @@ class MantidTests(unittest.TestCase):
 
         self.assertListEqual(function_defs_expected, function_defs)
 
-    def test_parseNistFunctionDefinitions_get_true_function(self):
-        prob = self.NIST_problem()
+    def test_parse_nist_function_definitions_get_true_function(self):
+        """
+        Test the NIST function definitions parsing
+        """
+        prob = nist_problem()
         nb_start_vals = 2
 
         function_defs = parse_function_definitions(prob, nb_start_vals)
@@ -126,6 +62,49 @@ class MantidTests(unittest.TestCase):
              "name=fitFunction,b1=250.0,b2=0.0005"]
 
         self.assertListEqual(function_defs_expected, function_defs)
+
+
+def neutron_problem():
+    """
+    Sets up the problem object for the neutron problem file:
+    ENGINX193749_calibration_peak19.txt
+    """
+
+    fname = get_file('FB_ENGINX193749_calibration_peak19.txt')
+    prob = FittingProblem(fname)
+    prob.name = 'ENGINX 193749 calibration, spectrum 651, peak 19'
+    prob.equation = ("name=LinearBackground,A0=0,A1=0;"
+                     "name=BackToBackExponential,"
+                     "I=597.076,A=1,B=0.05,X0=24027.5,S=22.9096")
+    prob.starting_values = None
+    prob.start_x = 23919.5789114
+    prob.end_x = 24189.3183142
+
+    return prob
+
+
+def create_wks_nist_problem_with_errors():
+    """
+    Helper function.
+    Creates a mantid workspace using the data provided by the
+    NIST problem Misra1a.
+    """
+    prob = nist_problem()
+    data_e = np.sqrt(abs(prob.data_y))
+    wks_exp = msapi.CreateWorkspace(DataX=prob.data_x, DataY=prob.data_y,
+                                    DataE=data_e)
+    return wks_exp
+
+
+def create_wks_nist_problem_without_errors():
+    """
+    Helper function.
+    Creates a mantid workspace using the data provided by the
+    NIST problem Misra1a.
+    """
+    prob = nist_problem()
+    wks_exp = msapi.CreateWorkspace(DataX=prob.data_x, DataY=prob.data_y)
+    return wks_exp
 
 
 if __name__ == "__main__":
