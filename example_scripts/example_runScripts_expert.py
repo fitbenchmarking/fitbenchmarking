3f7b3d3bf599041a759e475d8743400bb4dff465
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2019-09-24 15:52:28 +0000

    remove sys.setrecursionlimit calls

--- example_scripts/example_runScripts_expert.py
+++ example_scripts/example_runScripts_expert.py
@@ -10,12 +10,6 @@ import os
 import sys
 import glob
 
-
-# Avoid reaching the maximum recursion depth by setting recursion limit
-# This is useful when running multiple data set benchmarking
-# Otherwise recursion limit is reached and the interpreter throws an error
-sys.setrecursionlimit(10000)
-
 # Insert path to where the scripts are located, relative to
 # the example_scripts folder
 current_path = os.path.dirname(os.path.realpath(__file__))
