ec6d1ec230f9145d18979fa96eaef392e39390bc
Andrew Lister <andrew.lister@stfc.ac.uk>
2019-11-01 18:25:42 +0000

    Issue #264: Tidy up FitBenchmarking parser

--- fitbenchmarking/parsing/parse_fitbenchmark_data.py
+++ fitbenchmarking/parsing/parse_fitbenchmark_data.py
@@ -1,13 +1,11 @@
 from __future__ import (absolute_import, division, print_function)
 
 import os
-from fitbenchmarking.parsing import base_fitting_problem
+import mantid.simpleapi as msapi
 import numpy as np
 
+from fitbenchmarking.parsing import base_fitting_problem
 from fitbenchmarking.utils.logging_setup import logger
-from fitbenchmarking.parsing.fitbenchmark_data_functions import (
-    fitbenchmark_func_definitions
-)
 
 
 class FittingProblem(base_fitting_problem.BaseFittingProblem):
@@ -28,45 +26,33 @@ class FittingProblem(base_fitting_problem.BaseFittingProblem):
         super(FittingProblem, self).__init__(fname)
         super(FittingProblem, self).read_file()
 
-        entries = self.get_fitbenchmark_data_problem_entries(self.contents)
-        data_file_path = self.get_data_file(self.fname, entries['input_file'])
+        self._entries = self.get_fitbenchmark_data_problem_entries()
+        self._parsed_func = self.parse_function()
 
-        data_points = self.get_data_points(data_file_path)
+        self._name = self._entries['name']
+
+        data_points = self.get_data_points()
 
         self._data_x = data_points[:, 0]
         self._data_y = data_points[:, 1]
         self._data_e = data_points[:, 2]
-        self._name = entries['name']
-        self.function = None
 
         # String containing the function name(s) and the starting parameter values for each function
-        self._mantid_equation = entries['function']
+        self._mantid_equation = self._entries['function']
 
-        ##### Print number of equations until better way of doing this is looked at.
-        # Readable equation for output to user
-        #equation = entries['function'].split(';', 1)[-1]
-        #equation = equation.split(',', 1)[0]
+        self.function = self.fitbenchmark_func_definitions()
 
-        #self._equation = equation.split('=', 1)[1].strip()
-        equation_count = entries['function'].count(';') + 1
+        # Print number of equations until better way of doing this is looked at.
+        equation_count = self._entries['function'].count(';') + 1
         self._equation = '{} Functions'.format(equation_count)
 
-        # list of starting values in format [[name, [value1, value2, ...]], ...]
-        tmp_starting_values = entries['function'].split(';')
-        tmp_starting_values = (tmp.split('ties=')[0] for tmp in tmp_starting_values)
-        tmp_starting_values = ('f{}_{}'.format(i, sv.strip())
-                               for i, tmp in enumerate(tmp_starting_values)
-                               for sv in tmp.split(',')[1:]
-                               if sv.strip() != '' and not sv.startswith('BinWidth'))
-        self._starting_values = [[f.split('=')[0].strip(),
-                                  [float(f.split('=')[1].strip())]]
-                                 for f in tmp_starting_values]
+        self.starting_values = self.get_starting_values()
 
         # start and end values in x range
-        if 'fit_parameters' in entries:
-            self._start_x = entries['fit_parameters']['StartX']
-            self._end_x = entries['fit_parameters']['EndX']
-
+        if 'fit_parameters' in self._entries:
+            start_x, end_x = self.get_x_range()
+            self._start_x = start_x
+            self._end_x = end_x
         super(FittingProblem, self).close_file()
 
     def get_function(self):
@@ -75,64 +61,111 @@ class FittingProblem(base_fitting_problem.BaseFittingProblem):
         @returns :: function definition list containing the function and its
                     starting parameter values
         """
-        if self.function is None:
-            self.function = fitbenchmark_func_definitions(self._mantid_equation)
-            
         return self.function
 
-    def get_data_file(self, full_path_of_fitting_def_file, data_file_name):
+    def get_data_file(self):
         """
         Find/create the (full) path to a data_file specified in a FitBenchmark definition file, where
         the data_file is search for in the directory of the definition file and subfolders of this
         file
 
-        @param full_path_of_fitting_def_file :: (full) path of a FitBenchmark definition file
-        @param data_file_name :: the name of the data file as specified in the FitBenchmark definition file
-
         @returns :: (full) path to a data file (str). Return None if not found
         """
         data_file = None
+        data_file_name = self._entries['input_file']
         # find or search for path for data_file_name
-        for root, dirs, files in os.walk(os.path.dirname(full_path_of_fitting_def_file)):
+        for root, _, files in os.walk(os.path.dirname(self.fname)):
             for name in files:
                 if data_file_name == name:
                     data_file = os.path.join(root, data_file_name)
 
-        if data_file == None:
+        if data_file is None:
             logger.error("Data file {} not found".format(data_file_name))
 
         return data_file
 
-    def get_fitbenchmark_data_problem_entries(self, fname):
+    def get_fitbenchmark_data_problem_entries(self):
         """
-        Get the problem entries from a fitbenchmark problem definition file.
+        Get the problem self._entries from a fitbenchmark problem definition file.
 
-        @param fname :: path to the fitbenchmark problem definition file
-
-        @returns :: a dictionary with all the entires of the problem file
+        :returns: The entries from the file with string values
+        :rtype: dict
         """
 
         entries = {}
-        for line in fname:
+        for line in self.contents:
             # Discard comments
-            line = line.partition('#')[0]
+            line = line.split('#', 1)[0]
             line = line.rstrip()
             if not line:
                 continue
 
             lhs, rhs = line.split("=", 1)
-            entries[lhs.strip()] = eval(rhs.strip())
+            entries[lhs.strip()] = rhs.strip().strip('"').strip("'")
 
         return entries
 
-    def get_data_points(self, data_file_path):
+    def parse_function(self):
+        """
+        Get the params from the function as a list of dicts from the data file
+
+        :returns: Fucntion definition in format:
+                  [{name: [value1, value2, ...], ...]
+        :rtype: list of dict
+        """
+        function_def = []
+
+        functions = self._entries['function'].split(';')
+
+        for f in functions:
+            params_dict = {}
+            params_list = f.split(',')
+            pop_stack = False
+            stack = [params_dict]
+            for p in params_list:
+                name, val = p.split('=', 1)
+                name = name.strip()
+                val = val.strip()
+
+                if val[0] == '(':
+                    stack += [{}]
+                    val = val[1:]
+                if val[-1] == ')':
+                    if len(stack) == 1:
+                        raise ValueError('Could not parse.'
+                                         + 'Check parentheses in input')
+                    val = val[:-1]
+                    pop_stack = True
+
+                # Parse to an int/float if possible else assume string
+                tmp_val = None
+                for t in [int, float]:
+                    if tmp_val is None:
+                        try:
+                            tmp_val = t(val)
+                        except ValueError:
+                            pass
+
+                if tmp_val is not None:
+                    val = tmp_val
+
+                stack[-1][name] = val
+
+                if pop_stack:
+                    stack = stack[:-1]
+
+            function_def.append(params_dict)
+
+        return function_def
+
+    def get_data_points(self):
         """
         Get the data points of the problem from the data file.
 
-        @param data_file :: full path of the data file
         @return :: array of data points
         """
 
+        data_file_path = self.get_data_file()
         data_object = open(data_file_path, 'r')
         data_text = data_object.readlines()
 
@@ -147,3 +180,80 @@ class FittingProblem(base_fitting_problem.BaseFittingProblem):
             data_points[idx, :] = point
 
         return data_points
+
+    def get_starting_values(self):
+        """
+        Get the starting values for the problem
+
+        :returns: Starting values for the function
+        :rtype: list
+        """
+        ignore = ['name', 'BinWidth', 'ties']
+
+        starting_values = [['f{}_{}'.format(i, name), f[name]]
+                           for i, f in enumerate(self._parsed_func)
+                           for name in f.keys()
+                           if name not in ignore]
+
+        return starting_values
+
+    def get_x_range(self):
+        """
+        Get the x ranges for the problem
+
+        :returns: start_x and end_x
+        :rtype: float, float
+        """
+        fit_params_str = self._entries['fit_parameters'].strip('{').strip('}')
+        fit_params = fit_params_str.split(',')
+        for f in fit_params:
+            name, val = f.split(':')
+            name = name.strip().strip('"').strip("'")
+            if name not in ['StartX', 'EndX']:
+                continue
+
+            try:
+                val = float(val.strip())
+            except ValueError:
+                raise ValueError('Could not parse fit_parameter: {}'.format(f))
+
+            if name == 'StartX':
+                start_x = val
+            else:
+                end_x = val
+
+        return start_x, end_x
+
+    def fitbenchmark_func_definitions(self):
+        """
+        Processing the function in the FitBenchmark problem definition into a python
+        callable.
+
+        :returns: A function definition array of one element which contains a
+                callable Mantid function and the function parameter values.
+        :rtype: list
+        """
+        fit_function = None
+        param_vals = []
+
+        for f in self._parsed_func:
+            name = f['name']
+            params = {key: f[key]
+                      for key in f
+                      if key not in ['name', 'BinWidth', 'ties']}
+            param_vals.extend(params.values())
+            tmp_function = msapi.__dict__[name](**params)
+            if fit_function is None:
+                fit_function = tmp_function
+            else:
+                fit_function += tmp_function
+
+        for i, f in enumerate(self._parsed_func):
+            if 'ties' in f:
+                for tie, val in f['ties']:
+                    fit_function.tie(name='f{}.{}'.format(i, tie),
+                                     expr=val)
+
+        function_def = [[fit_function, param_vals]]
+
+        return function_def
