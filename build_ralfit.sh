e8b390c4470d034219992028f17ae41d548d6ef7
Michael Wathen <mwathen@gmail.com>
2019-11-04 14:18:20 +0000

    Adding LD_LIBRARY_PATH to RALFit installation

--- build_ralfit.sh
+++ build_ralfit.sh
@@ -12,4 +12,10 @@ make
 python setup.py build_ext
 python setup.py install
 
+export LD_LIBRARY_PATH=$SCRIPTPATH/RALFit/libRALFit/build:$LD_LIBRARY_PATH
+
 cd $SCRIPTPATH
+
+#! /usr/bin/env bash
+
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token 7u2X3sKhH6-HriIjYvtqPw" -d '{ "quiet": true }' https://api.travis-ci.org/job/607138661/debug
