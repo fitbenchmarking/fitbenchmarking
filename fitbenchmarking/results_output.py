72302ac7ac109793bb6ec6eb21f3f19e222fab1b
Michael Wathen <mwathen@gmail.com>
2019-11-18 15:10:20 +0000

    Including pandas table in fitbenchmarking/results_output.py

--- fitbenchmarking/results_output.py
+++ fitbenchmarking/results_output.py
@@ -3,12 +3,11 @@ Functions that creates the tables and the visual display pages.
 """
 
 from __future__ import (absolute_import, division, print_function)
-
+from collections import OrderedDict
 import logging
-
+import os
+import pandas as pd
 from fitbenchmarking.utils.logging_setup import logger
-from fitbenchmarking.resproc import numpy_restables
-from fitbenchmarking.resproc import rst_table
 from fitbenchmarking.resproc import visual_pages
 from fitbenchmarking.utils import create_dirs, options, misc
 
@@ -56,92 +55,30 @@ def save_results_tables(software_options, results_per_test, group_name,
     linked_problems = \
         visual_pages.create_linked_probs(results_per_test, group_name, results_dir)
 
-    acc_rankings, runtimes, _, _ = generate_tables(results_per_test, minimizers)
+    acc_tbl, acc_norm_tbl, acc_combined_table, \
+        runtime_tbl, runtime_norm_tbl, runtime_combined_table \
+        = generate_tables(results_per_test, minimizers, linked_problems)
 
-    acc_tbl = rst_table.create(columns_txt=minimizers,
-                               rows_txt=linked_problems,
-                               cells=acc_rankings,
-                               comparison_type='accuracy',
-                               comparison_dim='',
-                               using_errors=use_errors,
-                               color_scale=color_scale,
-                               comparison_mode=comparison_mode)
+    def colour_scheme(s):
+        '''
+        highlight the maximum in a Series yellow.
+        '''
+        is_max = s == s.min()
+        return ['background-color: white' if v else '' for v in is_max]
 
-    runtime_tbl = rst_table.create(columns_txt=minimizers,
-                                   rows_txt=linked_problems,
-                                   cells=runtimes,
-                                   comparison_type='runtime',
-                                   comparison_dim='',
-                                   using_errors=use_errors,
-                                   color_scale=color_scale,
-                                   comparison_mode=comparison_mode)
+    acc_norm_tbl = acc_norm_tbl.style.apply(colour_scheme, axis=1)
+    html = acc_norm_tbl.render()
 
-    save_tables(tables_dir, acc_tbl, use_errors, group_name,
+    save_tables(tables_dir, html, use_errors, group_name,
                 FILENAME_SUFFIX_ACCURACY)
-    save_tables(tables_dir, runtime_tbl, use_errors, group_name,
+    save_tables(tables_dir, html, use_errors, group_name,
                 FILENAME_SUFFIX_RUNTIME)
 
     # Shut down logging at end of run
     logging.shutdown()
 
 
-def create_acc_tbl(minimizers, linked_problems, accuracy_tbl, use_errors,
-                   color_scale, comparison_mode='abs'):
-    """
-    API function to create an accuracy table using the given parameters.
-    For usage, see the expert example script.
-
-    @param minimizers :: array of minimizers used in fitting
-    @param linked_problems :: array of the problems that were fitted
-    @param accuracy_tbl :: numpy array of the accuracy results
-    @param using_errors :: boolean whether to use errors or not
-    @param color_scale :: color scale for coloring the cells
-    @param comparison_mode :: str to select between 'abs', 'rel', 'both' for
-                              the style of comparison returned
-
-    @returns :: rst table of the results
-    """
-    # Save accuracy table for this group of fit problems
-    tbl_acc_indiv = rst_table.create(minimizers, linked_problems,
-                                     accuracy_tbl,
-                                     comparison_type='accuracy',
-                                     comparison_dim='',
-                                     using_errors=use_errors,
-                                     color_scale=color_scale,
-                                     comparison_mode=comparison_mode)
-
-    return tbl_acc_indiv
-
-
-def create_runtime_tbl(minimizers, linked_problems, runtime_tbl, use_errors,
-                       color_scale, comparison_mode='abs'):
-    """
-    API function to create a runtime table using the given parameters.
-    For usage, see the expert example script.
-
-    @param minimizers :: array of minimizers used in fitting
-    @param linked_problems :: array of the problems that were fitted
-    @param runtime_tbl :: numpy array of the runtime results
-    @param using_errors :: boolean whether to use errors or not
-    @param color_scale :: color scale for coloring the cells
-    @param comparison_mode :: str to select between 'abs', 'rel', 'both' for
-                              the style of comparison returned
-
-    @returns :: rst table of the results
-    """
-    # Save runtime table for this group of fit problems
-    tbl_runtime_indiv = rst_table.create(minimizers, linked_problems,
-                                         runtime_tbl,
-                                         comparison_type='runtime',
-                                         comparison_dim='',
-                                         using_errors=use_errors,
-                                         color_scale=color_scale,
-                                         comparison_mode=comparison_mode)
-
-    return tbl_runtime_indiv
-
-
-def generate_tables(results_per_test, minimizers):
+def generate_tables(results_per_test, minimizers, linked_problems):
     """
     Generates accuracy and runtime tables, with both normalised and absolute results, and summary tables.
 
@@ -150,42 +87,78 @@ def generate_tables(results_per_test, minimizers):
 
     @returns :: data and summary tables of the results as np arrays
     """
+    prev_name = ''
+    count = 1
+    acc_results_dict = OrderedDict()
+    time_results_dict = OrderedDict()
+    template = '<a target="_blank" href="{0}">{1}</a>'
+    for test_idx, prob_results in enumerate(results_per_test):
+        name = results_per_test[test_idx][0].problem.name
+        if name == prev_name:
+            count += 1
+        else:
+            count = 1
+        prev_name = name
+        prob_name = name + ' ' + str(count)
 
-    accuracy_tbl, runtime_tbl = \
-        numpy_restables.create_accuracy_runtime_tbls(results_per_test, minimizers)
+        name, url = linked_problems[test_idx].split('<')
+        linked_name = template.format(url.split('>')[0], prob_name)
 
-    norm_acc_rankings, norm_runtimes = \
-        numpy_restables.create_norm_tbls(accuracy_tbl, runtime_tbl)
+        acc_results_dict[linked_name] = [result.chi_sq for result in results_per_test[test_idx]]
+        time_results_dict[prob_name] = [result.runtime for result in results_per_test[test_idx]]
 
-    accuracy_tbl, runtime_tbl = numpy_restables.create_combined_tbls(abs_accuracy=accuracy_tbl,
-                                                                     rel_accuracy=norm_acc_rankings,
-                                                                     abs_runtime=runtime_tbl,
-                                                                     rel_runtime=norm_runtimes)
+    acc_tbl, acc_norm_tbl, acc_combined_table = create_tables(
+        acc_results_dict,
+        minimizers)
+    runtime_tbl, runtime_norm_tbl, runtime_combined_table = create_tables(
+        time_results_dict,
+        minimizers)
 
-    sum_cells_acc, sum_cells_runtime = \
-        numpy_restables.create_summary_tbls(accuracy_tbl, runtime_tbl)
+    return acc_tbl, acc_norm_tbl, acc_combined_table, runtime_tbl, runtime_norm_tbl, runtime_combined_table
 
-    return accuracy_tbl, runtime_tbl, sum_cells_acc, sum_cells_runtime
+
+def create_tables(table_data, minimizers):
+    """
+    Generates pandas tables.
+
+    :param table_data :: dictionary containing results
+    :type group_name :: dict
+    :param table_data :: list of minimizers (column headers)
+    :type group_name :: list
+
+
+    :return :: tuple(tbl, tbl_norm, tbl_combined) array of fitting results for
+                the problem group and the path to the results directory
+    :rtype :: (pandas DataFrame, pandas DataFrame, pandas DataFrame)
+    """
+
+    tbl = pd.DataFrame.from_dict(table_data, orient='index')
+    tbl.columns = minimizers
+
+    tbl_norm = tbl.apply(lambda x: x / x.min(), axis=1)
+
+    tbl_combined = tbl.copy()
+    for i in tbl_combined.columns:
+        tbl_combined[i] = tbl[i].map(str) + \
+            ' (' + tbl_norm[i].map(str) + ')'
+
+    return tbl, tbl_norm, tbl_combined
 
 
 def save_tables(tables_dir, table_data, use_errors, group_name, metric):
     """
     Helper function that saves the rst table both to html and to text.
-
     @param tables_dir :: the directory in which the tables are saved
     @param table_data :: the results table, in rst
     @param use_errors :: bool whether errors were used or not
     @param group_name :: name of the problem group
     @param metric :: whether it is an accuracy or runtime table
-
     @returns :: tables saved to both html and txt.
     """
-
-    rst_table.save_table_to_file(results_dir=tables_dir, table_data=table_data,
-                                 use_errors=use_errors, group_name=group_name,
-                                 metric_type=metric,
-                                 file_extension=FILENAME_EXT_TXT)
-    rst_table.save_table_to_file(results_dir=tables_dir, table_data=table_data,
-                                 use_errors=use_errors, group_name=group_name,
-                                 metric_type=metric,
-                                 file_extension=FILENAME_EXT_HTML)
+    values = {True: 'weighted', False: 'unweighted'}
+    table_name = ('{group_name}_{metric_type}_{weighted}_table.html'
+                  .format(weighted=values[use_errors],
+                          metric_type=metric, group_name=group_name))
+    file_name = os.path.join(tables_dir, table_name)
+    with open(file_name, 'w') as f:
+        f.write(table_data)
