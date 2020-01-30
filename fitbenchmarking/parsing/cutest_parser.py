d88f53dcc981e0eb00665d525bd926419d3fd180
Andrew Lister <andrew.lister@stfc.ac.uk>
2020-01-30 15:49:55 +0000

    Issue #334: Fix case where all files had same name (using id(None))

--- fitbenchmarking/parsing/cutest_parser.py
+++ fitbenchmarking/parsing/cutest_parser.py
@@ -19,6 +19,10 @@ from fitbenchmarking.utils.create_dirs import del_contents_of_dir
 from fitbenchmarking.utils.logging_setup import logger
 
 
+# Empty the cache
+del_contents_of_dir(os.environ["PYCUTEST_CACHE"])
+
+
 class CutestParser(Parser):
     """
     Use the pycutest interface to parse SIF files
@@ -31,20 +35,17 @@ class CutestParser(Parser):
         :return: The fully parsed fitting problem
         :rtype: fitbenchmarking.parsing.fitting_problem.FittingProblem
         """
-        self.cache_dir = TemporaryDirectory()
+        self.mastsif_dir = TemporaryDirectory()
 
         # set the MASTSIF environment variable so that pycutest
         # can find the sif files
-        os.environ["MASTSIF"] = self.cache_dir.name
+        os.environ["MASTSIF"] = self.mastsif_dir.name
 
         self._n = None
         self._m = None
 
         # get just the short filename (minus the .SIF)
         fp = FittingProblem()
-
-        # Clear the pycutest cache
-        del_contents_of_dir(os.environ['PYCUTEST_CACHE'])
 
         # Collect x and create new file with blank y
         fname, fp.data_x, fp.data_y = self._setup_data()
@@ -62,12 +63,12 @@ class CutestParser(Parser):
 
         # Use the object id as a hash to the function.
         # In order to prevent the id being reused also store the x_data
-        self._cache = {id(fp.data_x): {'f':self._p.objcons, 'x':fp.data_x}}
+        self._cache = {id(fp.data_x): {'f': self._p.objcons, 'x': fp.data_x}}
 
         return fp
 
     def __del__(self):
-        self.cache_dir.cleanup()
+        self.mastsif_dir.cleanup()
 
     def _import_problem(self, file_name):
         """
@@ -94,11 +95,17 @@ class CutestParser(Parser):
         :return: The result of evaluating at the given x
         :rtype: np.array
         """
+        os.environ["MASTSIF"] = self.mastsif_dir.name
+
         try:
             f = self._cache[id(x)]['f']
         except KeyError:
             fname, _, _ = self._setup_data(x)
-            p = self._import_problem(fname)
+            try:
+                p = self._import_problem(fname)
+            except:
+                print('Failed for: {}'.format(self._p.name))
+                raise
             f = p.objcons
             self._cache[id(x)] = {'f': f, 'x': x}
         _, fx = f(np.asarray(params))
@@ -125,6 +132,7 @@ class CutestParser(Parser):
         :returns: data_x, data_y
         :rtype: lists of floats
         """
+
         if self.file.closed:
             with open(self._filename, 'r') as f:
                 lines = f.readlines()
@@ -142,58 +150,65 @@ class CutestParser(Parser):
 
         written_x = False
 
-        file_path = os.path.join(self.cache_dir.name,
+        to_write = []
+
+        for line in lines:
+
+            if "IE M " in line:
+                if x is None:
+                    self._m = int(line.split()[2])
+                    # this will always come before x/y data
+                    # so allocate space for these now
+                    data_x = np.zeros(self._m)
+                    data_y = np.zeros(self._m)
+                    # initialize index parameters for x and y
+                else:
+                    line = line[:col_width-1] + str(len(x))
+                    self._m = len(x)
+
+            elif "IE N " in line:
+                self._n = int(line.split()[2])
+
+            elif "RE X" in line:
+                if x is None:
+                    data_x[x_idx] = float(line.split()[2])
+                    x_idx += 1
+                else:
+                    if written_x:
+                        continue
+                    else:
+                        line = ''
+                        new_lines = []
+                        for i, val in enumerate(x):
+                            idx = i+1
+                            tmp_line_x = ' RE X{}'.format(idx)
+                            spacing = ' '*(col_width - (5 + len(str(idx))))
+                            tmp_line_x += spacing + str(val)
+                            tmp_line_y = ' RE Y{}{}0.0'.format(idx,
+                                                                spacing)
+                            new_lines.extend([tmp_line_x, tmp_line_y])
+                        x_idx = y_idx = len(new_lines) / 2
+                        line = '\n'.join(new_lines)
+                        written_x = True
+
+            elif "RE Y" in line:
+                if x is None:
+                    data_y[y_idx] = float(line.split()[2])
+                    y_idx += 1
+                    line = line[:col_width-1] + '0.0\n'
+                else:
+                    continue
+
+            to_write.append(line)
+
+        if x is None:
+            x = data_x
+
+        file_path = os.path.join(self.mastsif_dir.name,
                                  '{}.SIF'.format(str(id(x))[-10:]))
 
-        with open(file_path, 'w') as tmp_file:
-            for line in lines:
-
-                if "IE M " in line:
-                    if x is None:
-                        self._m = int(line.split()[2])
-                        # this will always come before x/y data
-                        # so allocate space for these now
-                        data_x = np.zeros(self._m)
-                        data_y = np.zeros(self._m)
-                        # initialize index parameters for x and y
-                    else:
-                        line = line[:col_width-1] + str(len(x))
-                        self._m = len(x)
-
-                elif "IE N " in line:
-                    self._n = int(line.split()[2])
-
-                elif "RE X" in line:
-                    if x is None:
-                        data_x[x_idx] = float(line.split()[2])
-                        x_idx += 1
-                    else:
-                        if written_x:
-                            continue
-                        else:
-                            line = ''
-                            new_lines = []
-                            for i, val in enumerate(x):
-                                idx = i+1
-                                tmp_line_x = ' RE X{}'.format(idx)
-                                spacing = ' '*(col_width - (5 + len(str(idx))))
-                                tmp_line_x += spacing + str(val)
-                                tmp_line_y = ' RE Y{}{}0.0'.format(idx,
-                                                                   spacing)
-                                new_lines.extend([tmp_line_x, tmp_line_y])
-                            x_idx = y_idx = len(new_lines) / 2
-                            line = '\n'.join(new_lines)
-                            written_x = True
-
-                elif "RE Y" in line:
-                    if x is None:
-                        data_y[y_idx] = float(line.split()[2])
-                        y_idx += 1
-                        line = line[:col_width-1] + '0.0\n'
-                    else:
-                        continue
-
-                tmp_file.write(line)
+        with open(file_path, 'w') as f:
+            f.writelines(to_write)
 
         # check the data is right
         # TODO: turn into an exception
