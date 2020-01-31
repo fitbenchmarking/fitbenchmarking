bc0b8bd576880b2debc482ba9feae24928449060
Michael Wathen <mwathen@gmail.com>
2020-01-31 09:20:53 +0000

    Adding test placeholders for core module

---
+++ fitbenchmarking/core/tests/test_fitbenchmark_one_problem.py
@@ -0,0 +1,16 @@
+from __future__ import (absolute_import, division, print_function)
+import unittest
+
+import fitbenchmarking.core.fitbenchmark_one_problem
+
+
+class FitbmOneProbTests(unittest.TestCase):
+    pass
+
+
+class BenchmarkTests(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ fitbenchmarking/core/tests/test_fitting_benchmarking.py
@@ -0,0 +1,12 @@
+from __future__ import (absolute_import, division, print_function)
+import unittest
+
+import fitbenchmarking.core.fitting_benchmarking
+
+
+class FitbenchmarkGroup(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
