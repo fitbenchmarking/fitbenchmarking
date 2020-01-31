6720888ba4752e7be8e2650091b91d4df760d4f8
Michael Wathen <mwathen@gmail.com>
2020-01-31 09:26:36 +0000

    Adding test placeholders for results_processing module

---
+++ fitbenchmarking/results_processing/tests/test_tables.py
@@ -0,0 +1,28 @@
+from __future__ import (absolute_import, division, print_function)
+import unittest
+
+import fitbenchmarking.results_processing.tables
+
+
+class CreateResultsTablesTests(unittest.TestCase):
+    pass
+
+
+class GenerateTablesTests(unittest.TestCase):
+    pass
+
+
+class CreateResultsDictTests(unittest.TestCase):
+    pass
+
+
+class CreatePandasDataframeTests(unittest.TestCase):
+    pass
+
+
+class RenderPandasDataframeTests(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
