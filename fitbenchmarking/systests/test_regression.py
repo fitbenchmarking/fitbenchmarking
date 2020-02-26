8d78d3b2772de9d173d7aa585d98bcf0e7b80800
Andrew Lister <andrew.lister@stfc.ac.uk>
2020-02-26 14:46:05 +0000

    Issue #4: Seperate regression test for multifit

--- fitbenchmarking/systests/test_regression.py
+++ fitbenchmarking/systests/test_regression.py
@@ -44,6 +44,14 @@ class TestRegression(TestCase):
                                                'all_parsers_set'))
         run([problem], options_file=opt_file.name, debug=True)
 
+        opts.software = ['mantid']
+        opts.write(opt_file.name)
+        problem = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                               os.pardir,
+                                               'mock_problems',
+                                               'multifit_set'))
+        run([problem], options_file=opt_file.name, debug=True)
+
     def test_results_consistent(self):
         """
         Regression testing that the results of fitting a set of problems
@@ -53,7 +61,7 @@ class TestRegression(TestCase):
 
         expected_file = os.path.join(os.path.dirname(__file__),
                                      'expected_results',
-                                     'results_regression.txt')
+                                     'all_parsers.txt')
 
         actual_file = os.path.join(os.path.dirname(__file__),
                                    'results',
@@ -66,17 +74,57 @@ class TestRegression(TestCase):
         with open(actual_file, 'r') as f:
             actual = f.readlines()
 
-        diff = []
-        for exp_line, act_line in zip_longest(expected, actual):
-            exp_line = '' if exp_line is None else exp_line.strip('\n')
-            act_line = '' if act_line is None else act_line.strip('\n')
-            if exp_line != act_line:
-                diff.append([exp_line, act_line])
-
-        num_diff = min(6, len(diff))
-        msg = 'Accuracy has changed in at least 1 minimizer-' \
-              + 'problem pair. \n' \
-              + 'First {} of {} differences: \n'.format(num_diff, len(diff)) \
-              + '\n'.join(['{} \n{}'.format(*diff[i])
-                           for i in range(num_diff)])
+        diff, msg = diff_result(actual, expected)
         self.assertListEqual([], diff, msg)
+
+    def test_multifit_consistent(self):
+        """
+        Regression testing that the results of fitting multifit problems
+        against a single minimiser from mantid.
+        """
+
+        expected_file = os.path.join(os.path.dirname(__file__),
+                                     'expected_results',
+                                     'multifit.txt')
+
+        actual_file = os.path.join(os.path.dirname(__file__),
+                                   'results',
+                                   'multifit_set',
+                                   'multifit_set_acc_weighted_table.txt')
+
+        with open(expected_file, 'r') as f:
+            expected = f.readlines()
+
+        with open(actual_file, 'r') as f:
+            actual = f.readlines()
+
+        diff, msg = diff_result(actual, expected)
+        self.assertListEqual([], diff, msg)
+
+
+def diff_result(expected, actual):
+    """
+    Return the lines which differ between expected and actual along with a
+    formatted message.
+
+    :param expected: The expected result
+    :type expected: list of strings
+    :param actual: The actual result
+    :type actual: list of strings
+    :return: The lines which differ and a formatted message
+    :rtype: list of list of strings and str
+    """
+    diff = []
+    for exp_line, act_line in zip_longest(expected, actual):
+        exp_line = '' if exp_line is None else exp_line.strip('\n')
+        act_line = '' if act_line is None else act_line.strip('\n')
+        if exp_line != act_line:
+            diff.append([exp_line, act_line])
+
+    num_diff = min(6, len(diff))
+    msg = 'Accuracy has changed in at least 1 minimizer-' \
+          + 'problem pair. \n' \
+          + 'First {} of {} differences: \n'.format(num_diff, len(diff)) \
+          + '\n'.join(['{} \n{}'.format(*diff[i])
+                       for i in range(num_diff)])
+    return diff, msg
