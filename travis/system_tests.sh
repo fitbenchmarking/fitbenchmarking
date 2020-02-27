2e6248cd7fd06204fe35ff9eee8e9a759744d705
Andrew Lister <andrew.lister@stfc.ac.uk>
2020-02-27 10:27:45 +0000

    Issue #213: Test all versions of python3

---
+++ travis/system_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# ======= System Test =============== #
+pytest fitbenchmarking/systests
---
+++ travis/unit_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Test one dir to ogenerate the coverage file
+pytest fitbenchmarking/cli --cov=fitbenchmarking/cli --cov-report term-missing
+# Loop over other dirs to append to the coverage file
+for dir in controllers core parsing results_processing utils
+do
+    pytest fitbenchmarking/$dir --cov=fitbenchmarking/$dir --cov-report term-missing --cov-append
