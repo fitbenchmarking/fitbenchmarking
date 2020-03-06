7445d3964ef2e00f83d9602f922bb87e8563d883
Michael Wathen <mwathen@gmail.com>
2020-03-06 14:51:28 +0000

    Adding backports to mock modules

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,8 @@ sys.path.insert(0, os.path.abspath('../../'))
 # these pages.
 mock_modules = ['dfogn', 'dfols', 'pygsl', 'mantid', 'mantid.fitfunctions',
                 'iminuit', 'ral_nlls', 'bumps', 'bumps.fitters', 'bumps.names',
-                'pycutest']
+                'pycutest', 'backports', 'backports.tempfile',
+                'configparser', 'functools_lru_cache', 'cbook']
 for mod_name in mock_modules:
     sys.modules[mod_name] = mock.Mock()
 
