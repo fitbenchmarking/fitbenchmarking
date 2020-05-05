Fixing pylint errors in Jacobian class

fitbenchmarking/jacobian/numerical_2point_jacobian.py
@@ -1,19 +1,16 @@
 """
 Module which calculates SciPy 2 point finite difference approximation
 """
-from abc import ABCMeta, abstractmethod
-
 import numpy as np
 from scipy.optimize._numdiff import approx_derivative
 
 from fitbenchmarking.jacobian.base_jacobian import Jacobian
 
-
+# pylint: disable=useless-super-delegation
 class ScipyTwoPoint(Jacobian):
     """
     Implements SciPy 2 point finite difference approximation to the derivative
     """
-    __metaclass__ = ABCMeta
 
     def __init__(self, problem):
         super(ScipyTwoPoint, self).__init__(problem)

fitbenchmarking/jacobian/numerical_3point_jacobian.py
@@ -1,19 +1,16 @@
-from abc import ABCMeta, abstractmethod
-
-import numpy as np
 """
 Module which calculates SciPy 3 point finite difference approximation
 """
+import numpy as np
 from scipy.optimize._numdiff import approx_derivative
 
 from fitbenchmarking.jacobian.base_jacobian import Jacobian
 
-
+# pylint: disable=useless-super-delegation
 class ScipyThreePoint(Jacobian):
     """
     Implements SciPy 3 point finite difference approximation to the derivative
     """
-    __metaclass__ = ABCMeta
 
     def __init__(self, problem):
         super(ScipyThreePoint, self).__init__(problem)

fitbenchmarking/jacobian/numerical_cs_jacobian.py
@@ -1,20 +1,18 @@
 """
 Module which calculates SciPy complex steps numerical method
 """
-from abc import ABCMeta, abstractmethod
-
 import numpy as np
 from scipy.optimize._numdiff import approx_derivative
 
 from fitbenchmarking.jacobian.base_jacobian import Jacobian
 
 
+# pylint: disable=useless-super-delegation
 class ScipyCS(Jacobian):
     """
     Implements SciPy complex steps numerical method for calculating the
     derivative
     """
-    __metaclass__ = ABCMeta
 
     def __init__(self, problem):
         super(ScipyCS, self).__init__(problem)