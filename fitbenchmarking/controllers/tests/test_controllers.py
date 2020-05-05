Fixing unit tests for new jacobian class

fitbenchmarking/controllers/tests/test_controllers.py
@@ -17,6 +17,7 @@ from fitbenchmarking.controllers.scipy_controller import ScipyController
 from fitbenchmarking.parsing.parser_factory import parse_problem_file
 from fitbenchmarking.utils import exceptions
 from fitbenchmarking.utils.options import Options
+from fitbenchmarking.jacobian.numerical_2point_jacobian import ScipyTwoPoint
 
 
 def make_fitting_problem(file_name='cubic.dat'):
@@ -30,7 +31,8 @@ def make_fitting_problem(file_name='cubic.dat'):
 
     fitting_problem = parse_problem_file(fname, options)
     fitting_problem.correct_data()
-    return fitting_problem
+    jac = ScipyTwoPoint(fitting_problem)
+    return fitting_problem, jac
 
 
 class DummyController(Controller):
@@ -57,14 +59,14 @@ class BaseControllerTests(TestCase):
     """
 
     def setUp(self):
-        self.problem = make_fitting_problem()
+        self.problem, self.jac = make_fitting_problem()
 
     def test_data(self):
         """
         BaseSoftwareController: Test data is read into controller correctly
         """
 
-        controller = DummyController(self.problem)
+        controller = DummyController(self.problem, self.jac)
 
         if self.problem.start_x is not None:
             assert min(controller.data_x) >= self.problem.start_x
@@ -88,7 +90,7 @@ class BaseControllerTests(TestCase):
         """
         BaseSoftwareController: Test prepare function
         """
-        controller = DummyController(self.problem)
+        controller = DummyController(self.problem, self.jac)
         controller.minimizer = 'test'
         controller.parameter_set = 0
         controller.prepare()
@@ -98,7 +100,7 @@ class BaseControllerTests(TestCase):
         """
         BaseSoftwareController: Test eval_chisq function
         """
-        controller = DummyController(self.problem)
+        controller = DummyController(self.problem, self.jac)
 
         params = np.array([1, 2, 3, 4])
         x = np.array([6, 2, 32, 4])
@@ -113,7 +115,7 @@ class BaseControllerTests(TestCase):
         """
         BaseSoftwareController: Test eval_chisq function
         """
-        controller = DummyController(self.problem)
+        controller = DummyController(self.problem, self.jac)
 
         params = np.array([1, 2, 3, 4])
         x = np.array([6, 2, 32, 4])
@@ -129,7 +131,7 @@ class BaseControllerTests(TestCase):
         BaseSoftwareController: Test check_attributes function for flag
                                 attribute
         """
-        controller = DummyController(self.problem)
+        controller = DummyController(self.problem, self.jac)
         controller.flag = 1
         controller.check_attributes()
 
@@ -138,7 +140,7 @@ class BaseControllerTests(TestCase):
         BaseSoftwareController: Test check_attributes function for flag
                                 attribute
         """
-        controller = DummyController(self.problem)
+        controller = DummyController(self.problem, self.jac)
         with self.assertRaises(exceptions.ControllerAttributeError):
             controller.check_attributes()
 
@@ -153,13 +155,13 @@ class ControllerTests(TestCase):
     """
 
     def setUp(self):
-        self.problem = make_fitting_problem()
+        self.problem, self.jac = make_fitting_problem()
 
     def test_mantid(self):
         """
         MantidController: Test for output shape
         """
-        controller = MantidController(self.problem)
+        controller = MantidController(self.problem, self.jac)
         controller.minimizer = 'Levenberg-Marquardt'
         self.shared_testing(controller)
 
@@ -175,9 +177,9 @@ class ControllerTests(TestCase):
         MantidController: Additional bespoke test for multifit
         """
         file_path = os.path.join('multifit_set', 'multifit.txt')
-        problem = make_fitting_problem(file_path)
+        problem, jac = make_fitting_problem(file_path)
 
-        controller = MantidController(problem)
+        controller = MantidController(problem, jac)
         controller.minimizer = 'Levenberg-Marquardt'
 
         controller.parameter_set = 0
@@ -197,8 +199,8 @@ class ControllerTests(TestCase):
         Test the override in Mantid conroller is working correctly for
         evaluating chi_squared (SingleFit).
         """
-        m_controller = MantidController(self.problem)
-        b_controller = DummyController(self.problem)
+        m_controller = MantidController(self.problem, self.jac)
+        b_controller = DummyController(self.problem, self.jac)
         params = np.array([1, 2, 3, 4])
         x = np.array([6, 2, 32, 4])
         y = np.array([1, 21, 3, 4])
@@ -216,8 +218,8 @@ class ControllerTests(TestCase):
         Test the override in Mantid conroller is working correctly for
         evaluating chi_squared (MultiFit).
         """
-        m_controller = MantidController(self.problem)
-        b_controller = DummyController(self.problem)
+        m_controller = MantidController(self.problem, self.jac)
+        b_controller = DummyController(self.problem, self.jac)
         params = [np.array([1, 2, 3, 4]),
                   np.array([1, 2, 3, 4]),
                   np.array([1, 2, 3, 4])]
@@ -244,7 +246,7 @@ class ControllerTests(TestCase):
         """
         SasviewController: Test for output shape
         """
-        controller = SasviewController(self.problem)
+        controller = SasviewController(self.problem, self.jac)
         controller.minimizer = 'amoeba'
         self.shared_testing(controller)
 
@@ -259,7 +261,7 @@ class ControllerTests(TestCase):
         """
         ScipyController: Test for output shape
         """
-        controller = ScipyController(self.problem)
+        controller = ScipyController(self.problem, self.jac)
         controller.minimizer = 'lm'
         self.shared_testing(controller)
 
@@ -274,7 +276,7 @@ class ControllerTests(TestCase):
         """
         DFOController: Tests for output shape
         """
-        controller = DFOController(self.problem)
+        controller = DFOController(self.problem, self.jac)
         # test one from each class
         minimizers = ['dfogn',
                       'dfols']
@@ -293,7 +295,7 @@ class ControllerTests(TestCase):
         """
         GSLController: Tests for output shape
         """
-        controller = GSLController(self.problem)
+        controller = GSLController(self.problem, self.jac)
         # test one from each class
         minimizers = ['lmsder',
                       'nmsimplex',
@@ -313,7 +315,7 @@ class ControllerTests(TestCase):
         """
         RALFitController: Tests for output shape
         """
-        controller = RALFitController(self.problem)
+        controller = RALFitController(self.problem, self.jac)
         minimizers = ['gn', 'gn_reg', 'hybrid', 'hybrid_reg']
         for minimizer in minimizers:
             controller.minimizer = minimizer
@@ -328,7 +330,7 @@ class ControllerTests(TestCase):
         """
         MinuitController: Tests for output shape
         """
-        controller = MinuitController(self.problem)
+        controller = MinuitController(self.problem, self.jac)
         controller.minimizer = 'minuit'
         self.shared_testing(controller)
         controller._status = 0

fitbenchmarking/results_processing/tests/test_performance_profiler.py
@@ -9,6 +9,7 @@ from collections import OrderedDict
 import numpy as np
 
 from fitbenchmarking.core.results_output import preproccess_data
+from fitbenchmarking.jacobian.numerical_2point_jacobian import ScipyTwoPoint
 from fitbenchmarking.parsing.fitting_problem import FittingProblem
 from fitbenchmarking.results_processing import performance_profiler
 from fitbenchmarking.utils.fitbm_result import FittingResult
@@ -23,7 +24,7 @@ class PerformanceProfillerTests(unittest.TestCase):
     def generate_mock_results(self):
         """
         Generates results to test against
-        
+
         :return: A list of results objects along with expected values for
                  normallised accuracy and runtimes
         :rtype: tuple(list of FittingResults,
@@ -57,10 +58,12 @@ class PerformanceProfillerTests(unittest.TestCase):
             runtime_expected.append(
                 list(runtime_results) / np.min(runtime_results))
             prob_results = []
+            jac = ScipyTwoPoint(problem)
             for j in range(self.num_minimizers):
                 minimizer = 'min_{}'.format(j)
                 prob_results.append(FittingResult(options=options,
                                                   problem=problem,
+                                                  jac=jac,
                                                   initial_params=[1, 2, 3],
                                                   params=[1, 2, 3],
                                                   chi_sq=acc_results[j],

fitbenchmarking/results_processing/tests/test_plots.py
@@ -9,6 +9,7 @@ except ImportError:
 import unittest
 
 from fitbenchmarking.parsing.fitting_problem import FittingProblem
+from fitbenchmarking.jacobian.numerical_2point_jacobian import ScipyTwoPoint
 from fitbenchmarking.utils.fitbm_result import FittingResult
 from fitbenchmarking.utils.options import Options
 from fitbenchmarking.results_processing import plots
@@ -18,6 +19,7 @@ class PlotTests(unittest.TestCase):
     """
     Test the plot object is correct.
     """
+
     def setUp(self):
         self.opts = Options()
         self.opts.use_errors = True
@@ -28,10 +30,12 @@ class PlotTests(unittest.TestCase):
         self.prob.data_y = np.array([4, 3, 5, 2, 1])
         self.prob.data_e = np.array([0.5, 0.2, 0.3, 0.1, 0.4])
         self.prob.starting_values = [{'x': 1, 'y': 2}]
-        self.prob.eval_f = lambda x, y: x[0]*y + x[1]
+        self.prob.eval_f = lambda x, y: x[0] * y + x[1]
         self.prob.name = 'full name'
+        jac = ScipyTwoPoint(self.prob)
         self.fr = FittingResult(options=self.opts,
                                 problem=self.prob,
+                                jac=jac,
                                 chi_sq=1.0,
                                 initial_params=[1.8],
                                 params=[1.2],

fitbenchmarking/utils/tests/test_fitbm_result.py
@@ -9,6 +9,7 @@ import numpy as np
 
 from fitbenchmarking import mock_problems
 from fitbenchmarking.parsing.parser_factory import parse_problem_file
+from fitbenchmarking.jacobian.numerical_2point_jacobian import ScipyTwoPoint
 from fitbenchmarking.utils.fitbm_result import FittingResult
 from fitbenchmarking.utils.options import Options
 
@@ -33,9 +34,10 @@ class FitbmResultTests(unittest.TestCase):
         self.runtime = 0.01
         self.params = np.array([1, 3, 4, 4])
         self.initial_params = np.array([0, 0, 0, 0])
+        self.jac = ScipyTwoPoint(self.problem)
         self.result = FittingResult(
-            options=self.options, problem=self.problem, chi_sq=self.chi_sq,
-            runtime=self.runtime, minimizer=self.minimizer,
+            options=self.options, problem=self.problem, jac=self.jac,
+            chi_sq=self.chi_sq, runtime=self.runtime, minimizer=self.minimizer,
             initial_params=self.initial_params, params=self.params,
             error_flag=0)
 
@@ -53,7 +55,7 @@ class FitbmResultTests(unittest.TestCase):
         self.result.set_colour_scale()
 
         r = self.problem.eval_r(self.params)
-        min_test = np.matmul(self.problem.eval_j(self.params).T, r)
+        min_test = np.matmul(self.jac.eval(self.params).T, r)
         self.norm_rel = np.linalg.norm(min_test) / np.linalg.norm(r)
 
     def test_init_with_dataset_id(self):
@@ -64,7 +66,7 @@ class FitbmResultTests(unittest.TestCase):
                   np.array([2, 3, 57, 8]),
                   np.array([4, 2, 5, 1])]
         initial_params = np.array([0, 0, 0, 0])
-        
+
         self.problem.data_x = [np.array([3, 2, 1, 4]),
                                np.array([5, 1, 2, 3]),
                                np.array([6, 7, 8, 1])]
@@ -77,9 +79,10 @@ class FitbmResultTests(unittest.TestCase):
         self.problem.sorted_index = [np.array([2, 1, 0, 3]),
                                      np.array([1, 2, 3, 0]),
                                      np.array([3, 0, 1, 2])]
+        jac = ScipyTwoPoint(self.problem)
         self.result = FittingResult(
-            options=self.options, problem=self.problem, chi_sq=chi_sq,
-            runtime=runtime, minimizer=minimizer,
+            options=self.options, problem=self.problem, jac=jac,
+            chi_sq=chi_sq, runtime=runtime, minimizer=minimizer,
             initial_params=initial_params, params=params,
             error_flag=0, dataset_id=1)
 