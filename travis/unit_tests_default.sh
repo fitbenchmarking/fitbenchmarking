6601e3f93b0c235a2ac72a27b187bf4e3503ba0d
Michael Wathen <mwathen@gmail.com>
2020-05-27 12:14:53 +0100

    Fixing default unit tests on travis

--- travis/unit_tests_default.sh
+++ travis/unit_tests_default.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Test default installation
 pytest fitbenchmarking/cli --cov=fitbenchmarking/cli --cov-report term-missing
+status=$?
 for dir in controllers core jacobian parsing results_processing utils
 do
     pytest fitbenchmarking/$dir --test-type default
+    status=$(($status + $?))
 done
+if [[ $status != 0 ]]
+then
+   exit 1
+fi
 
 exit 0
