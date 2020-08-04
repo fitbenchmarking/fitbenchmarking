65f397f2ee9b8c36610c5a4a3a9b7a1262b42f7e
Michael Wathen <mwathen@gmail.com>
2020-08-04 12:36:40 +0100

    Fixing system tests

--- fitbenchmarking/controllers/mantid_controller.py
+++ fitbenchmarking/controllers/mantid_controller.py
@@ -106,8 +106,8 @@ class MantidController(Controller):
         """
         Mantid cannot use Jacobian information
         """
-        has_jacobian = False
-        jacobian_free_solvers = []
+        has_jacobian = True
+        jacobian_free_solvers = ["Simplex"]
         return has_jacobian, jacobian_free_solvers
 
     def setup(self):
