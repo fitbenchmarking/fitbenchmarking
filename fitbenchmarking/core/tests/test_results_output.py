e66dc895bd816b11f6291aad1567094b0632fef0
Michael Wathen <mwathen@gmail.com>
2020-08-05 12:16:53 +0100

    Adding tests for results_output.create_directories

--- fitbenchmarking/core/tests/test_results_output.py
+++ fitbenchmarking/core/tests/test_results_output.py
@@ -1,7 +1,10 @@
 from __future__ import (absolute_import, division, print_function)
 import unittest
+import os
+import shutil
 
-import fitbenchmarking.core.results_output
+from fitbenchmarking.core.results_output import create_directories
+from fitbenchmarking.utils.options import Options
 
 
 class SaveResultsTests(unittest.TestCase):
@@ -13,11 +16,49 @@ class SaveResultsTests(unittest.TestCase):
 
 
 class CreateDirectoriesTests(unittest.TestCase):
-    def test_dummy(self):
+    """
+    Unit tests for create_directories function
+    """
+
+    def setUp(self):
+        """
+        Setting up paths and results folders
+        """
+        self.options = Options()
+        test_path = os.path.dirname(os.path.realpath(__file__))
+        self.dirname = os.path.join(test_path, 'results')
+        os.mkdir(self.dirname)
+
+    def tearDown(self):
+        """
+        Clean up created folders.
+        """
+        shutil.rmtree(self.dirname)
+
+    def test_create_dirs_correct(self):
         """
         Dummy test to appease pytest
         """
-        pass
+        group_name = 'test_group'
+        self.options.results_dir = self.dirname
+        expected_results_dir = self.dirname
+        expected_group_dir = os.path.join(expected_results_dir, group_name)
+        expected_support_dir = os.path.join(expected_group_dir,
+                                            'support_pages')
+        expected_figures_dir = os.path.join(expected_support_dir, 'figures')
+
+        results_dir, group_dir, support_dir, figures_dir = \
+            create_directories(self.options, group_name)
+
+        assert results_dir == expected_results_dir
+        assert group_dir == expected_group_dir
+        assert support_dir == expected_support_dir
+        assert figures_dir == expected_figures_dir
+
+        assert os.path.isdir(results_dir)
+        assert os.path.isdir(group_dir)
+        assert os.path.isdir(support_dir)
+        assert os.path.isdir(figures_dir)
 
 
 class PreproccessDataTests(unittest.TestCase):
