Fixing constant within Jacobian error

fitbenchmarking/parsing/nist_data_functions.py
@@ -63,9 +63,11 @@ def nist_jacobian_definition(jacobian, param_names):
         # Create a function def for each starting set in startvals
         if not is_safe(jacobian_scipy_format):
             raise ParsingError('Error while sanitizing Jacobian input')
-        # Sanitizing of function_scipy_format is done so exec use is valid
-        # Param_names is sanitized in get_nist_param_names_and_values
-        # pylint: disable=exec-used
+
+        # Checks to see if the value is an integer and if so reformats the
+        # value to be a constant vector.
+        if is_int(jacobian_scipy_format):
+            jacobian_scipy_format += "*(np.ones(x.shape[0]))"
         scipy_jacobian.append(jacobian_scipy_format)
     jacobian_format = "-np.array([{}]).T".format(",".join(scipy_jacobian))
 
@@ -74,12 +76,33 @@ def nist_jacobian_definition(jacobian, param_names):
         jacobian_format = jacobian_format.replace(
             name, "{0}[{1}]".format(new_param_name, i))
 
+    # Sanitizing of jacobian_scipy_format is done so exec use is valid
+    # Param_names is sanitized in get_nist_param_names_and_values
+    # pylint: disable=exec-used
     local_dict = {}
     global_dict = {'__builtins__': {}, 'np': np}
     exec("def jacobian_function(x, " + new_param_name + "): return "
          + jacobian_format, global_dict, local_dict)
 
     return local_dict['jacobian_function']
+
+
+def is_int(value):
+    """
+    Checks to see if a value is an integer or not
+
+    :param value: String representation of an equation
+    :type value: str
+
+    :return: Whether or not value is an int
+    :rtype: bool
+    """
+    try:
+        int(value)
+        value_bool = True
+    except ValueError:
+        value_bool = False
+    return value_bool
 
 
 def format_function_scipy(function):