25591e161c1beb8e914e32bc1e3538da74f76388
Michael Wathen <mwathen@gmail.com>
2020-08-21 11:09:44 +0100

    Updating docker travis file

--- travis/unit_tests_default.sh
+++ travis/unit_tests_default.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 # Test default installation
-pytest fitbenchmarking/cli --cov=fitbenchmarking/cli --cov-report term-missing
+if [ -z "$1" ]
+    then
+        FB_DIR=fitbenchmarking
+    else
+        FB_DIR=$1/fitbenchmarking
+fi
+pytest $FB_DIR/cli --cov=fitbenchmarking/cli --cov-report term-missing
 status=$?
 for dir in controllers core jacobian parsing results_processing utils
 do
-    pytest fitbenchmarking/$dir --test-type default
+    pytest $FB_DIR/$dir --test-type default
     status=$(($status + $?))
 done
 if [[ $status != 0 ]]
