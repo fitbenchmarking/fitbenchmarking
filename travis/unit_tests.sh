59f5e23b7d428e3d114bf888a87a31d342bf6871
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2020-09-07 11:38:19 +0100

    change directory of fitbenchmarking

--- travis/unit_tests.sh
+++ travis/unit_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Test one dir to generate the coverage file
-pytest fitbenchmarking/cli --cov=fitbenchmarking/cli --cov-report term-missing
+pytest fitbenchmarking/fitbenchmarking/cli --cov=fitbenchmarking/cli --cov-report term-missing
 status=$?
 # Loop over other dirs to append to the coverage file
 for dir in controllers core jacobian parsing results_processing utils
 do
-    pytest fitbenchmarking/$dir --cov=fitbenchmarking/$dir --cov-report term-missing --cov-append
+    pytest fitbenchmarking/fitbenchmarking/$dir --cov=fitbenchmarking/fitbenchmarking/$dir --cov-report term-missing --cov-append
     status=$(($status + $?))
 done
 
