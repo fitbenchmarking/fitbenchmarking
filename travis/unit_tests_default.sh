00f7e1f740ebc79aa7321f12f2bf923a256e3cae
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2020-09-16 15:03:15 +0100

    revert to fixed test directory

--- travis/unit_tests_default.sh
+++ travis/unit_tests_default.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 # Test default installation
-if [ -z "$1" ]
-    then
-        FB_DIR=fitbenchmarking
-    else
-        FB_DIR=$1/fitbenchmarking
-fi
-pytest $FB_DIR/cli --cov=fitbenchmarking/cli --cov-report term-missing
+pytest fitbenchmarking/cli --cov=fitbenchmarking/cli --cov-report term-missing
 status=$?
 for dir in controllers core jacobian parsing results_processing utils
 do
-    pytest $FB_DIR/$dir --test-type default
+    pytest fitbenchmarking/$dir --test-type default
     status=$(($status + $?))
 done
 if [[ $status != 0 ]]
