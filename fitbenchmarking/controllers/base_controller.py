d1c10a6f62f6f112d77bf9aa95498d177f3e4ff6
jess-farmer <jessica.farmer@stfc.ac.uk>
2021-06-23 13:24:03 +0100

    fix flake8 errors

--- fitbenchmarking/controllers/base_controller.py
+++ fitbenchmarking/controllers/base_controller.py
@@ -33,12 +33,15 @@ class Controller:
 
         - ``all`` - all minimizers
         - ``ls`` - least-squares fitting algorithms
-        - ``deriv_free`` - derivative free algorithms (these are algorithms that cannot use
-          information about derivatives -- e.g., the ``Simplex`` method in ``Mantid``)
+        - ``deriv_free`` - derivative free algorithms (these are algorithms
+          that cannot use information about derivatives -- e.g., the
+          ``Simplex`` method in ``Mantid``)
         - ``general`` - minimizers which solve a generic `min f(x)`
-        - ``simplex`` - derivative free simplex based algorithms e.g. Nelder-Mead
+        - ``simplex`` - derivative free simplex based algorithms
+          e.g. Nelder-Mead
         - ``trust_region`` - algorithms which emply a trust region approach
-        - ``levenberg-marquardt`` - minimizers that use the Levenberg Marquardt algorithm
+        - ``levenberg-marquardt`` - minimizers that use the
+          Levenberg-Marquardt algorithm
         - ``gauss_newton`` - minimizers that use the Gauss Newton algorithm
         - ``bfgs`` - minimizers that use the BFGS algorithm
         - ``conjugate_gradient`` - Conjugate Gradient algorithms
