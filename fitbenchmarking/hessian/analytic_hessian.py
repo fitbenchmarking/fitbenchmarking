6228cc1fff4e9870534f1bf82115186b5a2589e8
jess-farmer <jessica.farmer@stfc.ac.uk>
2021-07-15 16:22:15 +0100

    Add weighted_nlls hessian

--- fitbenchmarking/hessian/analytic_hessian.py
+++ fitbenchmarking/hessian/analytic_hessian.py
@@ -33,17 +33,21 @@ class Analytic(Hessian):
 
         x = kwargs.get("x", self.problem.data_x)
         # y = kwargs.get("y", self.problem.data_y)
-        # e = kwargs.get("e", self.problem.data_e)
+        e = kwargs.get("e", self.problem.data_e)
+        J = self.jacobian.eval(params, **kwargs)
         rx = self.cached_func_values(self.cost_func.cache_rx,
                                      self.cost_func.eval_r,
                                      params,
                                      **kwargs)
         grad2_r = self.problem.hessian(x, params)
-        # if self.problem.options.cost_func_type == "weighted_nlls":
-        #     # scales each column of the Jacobian by the weights
-        #     hes[:,:,0] = hes[:,:,0] / e[0]
+        if self.problem.options.cost_func_type == "weighted_nlls":
+            # scales the Hessian by the weights
+            for i in range(len(e)):
+                grad2_r[:,:,i] = grad2_r[:,:,i]/ e[i]
+
         hes = matmul(grad2_r, rx)
-        return hes
+        print('test2')
+        return hes, J
 
     def eval_cost(self, params, **kwargs):
         """
@@ -55,7 +59,6 @@ class Analytic(Hessian):
         :return: Computed derivative of the cost function
         :rtype: numpy array
         """
-        H = self.eval(params, **kwargs)
-        J = self.jacobian.eval(params, **kwargs)
+        H, J = self.eval(params, **kwargs)
         out = H + matmul(np.transpose(J), J)
         return out
