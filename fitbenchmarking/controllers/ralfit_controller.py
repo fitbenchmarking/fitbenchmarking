fix hessian eval errors and failing tests

fitbenchmarking/controllers/ralfit_controller.py
@@ -87,6 +87,8 @@ class RALFitController(Controller):
 
         if self.hessian:
             self._options[b"exact_second_derivatives"] = True
+        else:
+            self._options[b"exact_second_derivatives"] = False
 
         # If parameter ranges have been set in problem, then set up bounds
         # option. For RALFit, this must be a 2 tuple array like object,
@@ -100,6 +102,24 @@ class RALFitController(Controller):
                 [-np.inf]*len(self.initial_params),
                 [np.inf]*len(self.initial_params))
 
+    # pylint: disable=unused-argument
+    def hes_eval(self, params, r):
+        """
+        Function to ensure correct inputs and outputs
+        are used for the RALFit hessian evalution
+
+        :param params: parameters
+        :type params: numpy array
+        :param r: resuiduals, required by RALFit to
+                  be passed to hessian evaluation
+        :type r: numpy array
+        :return: hessian evaluation from hessian.eval
+        :rtype: numpy array
+        """
+        hes, _ = self.hessian.eval(params)
+        return hes
+    # pylint: enable=unused-argument
+
     def fit(self):
         """
         Run problem with RALFit.
@@ -108,7 +128,7 @@ class RALFitController(Controller):
             self._popt = ral_nlls.solve(self.initial_params,
                                         self.cost_func.eval_r,
                                         self.jacobian.eval,
-                                        self.hessian.eval,
+                                        self.hes_eval,
                                         options=self._options,
                                         lower_bounds=self.param_ranges[0],
                                         upper_bounds=self.param_ranges[1])[0]