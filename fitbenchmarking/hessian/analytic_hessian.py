1911769990663490d357bdc044c1e3ba3c195fae
jess-farmer <jessica.farmer@stfc.ac.uk>
2021-08-16 09:40:49 +0100

    move jacobian and eval_model statements

--- fitbenchmarking/hessian/analytic_hessian.py
+++ fitbenchmarking/hessian/analytic_hessian.py
@@ -35,24 +35,26 @@ class Analytic(Hessian):
         y = kwargs.get("y", self.problem.data_y)
         e = kwargs.get("e", self.problem.data_e)
         grad2_r = self.problem.hessian(x, params)
+        J = self.jacobian.eval(params, **kwargs)
         if self.problem.options.cost_func_type == "weighted_nlls":
             # scales the Hessian by the weights
             for i in range(len(e)):
                 grad2_r[:, :, i] = grad2_r[:, :, i] / e[i]
         if self.problem.options.cost_func_type == "hellinger_nlls":
-            J = self.jacobian.eval(params, **kwargs)
+            model_eval = self.problem.eval_model(params, x=x)
             for i in range(len(x)):
                 grad2_r[:, :, i] = 1/2*(
-                    self.problem.eval_model(params, x=x)**(-1/2))[i]\
+                    model_eval[i]**(-1/2))\
                     * grad2_r[:, :, i] - \
-                    1/2*(self.problem.eval_model(params, x=x)**(-3/2))[i]\
+                    1/2*(model_eval[i]**(-3/2))\
                     * np.matmul(J.T, J)
         if self.problem.options.cost_func_type == "poisson":
+            model_eval = self.problem.eval_model(params, x=x)
             J = self.jacobian.eval_cost(params, **kwargs)
             for i in range(len(x)):
                 grad2_r[:, :, i] = grad2_r[:, :, i]\
-                    * (1-y/self.problem.eval_model(params, x=x))[i]\
-                    + (y/(self.problem.eval_model(params, x=x)**2))[i]\
+                    * (1-y[i]/model_eval[i])\
+                    + (y[i]/model_eval[i]**2)\
                     * np.matmul(J.T, J)
             hes = np.sum(grad2_r, 2)
 
