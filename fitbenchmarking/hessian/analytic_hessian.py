97a4db2ef65832e43c0ae55d607814a4be063126
jess-farmer <jessica.farmer@stfc.ac.uk>
2021-08-17 16:27:25 +0100

    Fix tests

--- fitbenchmarking/hessian/analytic_hessian.py
+++ fitbenchmarking/hessian/analytic_hessian.py
@@ -34,7 +34,7 @@ class Analytic(Hessian):
         x = kwargs.get("x", self.problem.data_x)
         y = kwargs.get("y", self.problem.data_y)
         e = kwargs.get("e", self.problem.data_e)
-        grad2_r = self.problem.hessian(x, params)
+        grad2_r = -self.problem.hessian(x, params)
         J = self.jacobian.eval(params, **kwargs)
         if self.problem.options.cost_func_type == "weighted_nlls":
             # scales the Hessian by the weights
@@ -42,20 +42,16 @@ class Analytic(Hessian):
                 grad2_r[:, :, i] = grad2_r[:, :, i] / e[i]
         if self.problem.options.cost_func_type == "hellinger_nlls":
             model_eval = self.problem.eval_model(params, x=x)
-            for i in range(len(x)):
-                grad2_r[:, :, i] = 1/2*(
-                    model_eval[i]**(-1/2))\
-                    * grad2_r[:, :, i] + \
-                    model_eval[i]**(-1/2)\
-                    * np.matmul(J.T, J)
+            for i, (f, j) in enumerate(zip(model_eval, J)):
+                j = np.array([j])
+                grad2_r[:, :, i] = 1/2 * f**(-1/2) * grad2_r[:, :, i] \
+                    + f**(-1/2) * np.matmul(j.T, j)
         if self.problem.options.cost_func_type == "poisson":
             model_eval = self.problem.eval_model(params, x=x)
-            J = self.jacobian.eval_cost(params, **kwargs)
-            for i in range(len(x)):
-                grad2_r[:, :, i] = grad2_r[:, :, i]\
-                    * (1-y[i]/model_eval[i])\
-                    + (y[i]/model_eval[i]**2)\
-                    * np.matmul(J.T, J)
+            for i, (f, j) in enumerate(zip(model_eval, J)):
+                j = np.array([j])
+                grad2_r[:, :, i] = -(grad2_r[:, :, i] * (1-y[i]/f)
+                                     + (y[i]/f**2)*np.matmul(j.T, j))
             hes = np.sum(grad2_r, 2)
 
         if self.problem.options.cost_func_type != "poisson":
