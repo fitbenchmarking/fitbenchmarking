fix jacobian (still in progress)

examples/benchmark_problems/Data_Assimilation/functions/lorentz.py
@@ -29,13 +29,15 @@ def lorentz3d(t, x, sigma, r, b):
 
     raise ValueError('x is the wrong shape in lorentz3d call.')
 
-def lorentz3d_jac(x, params):
 
-    if len(x.shape) == 1:
-        return np.array([[
-            -params[0], params[0], 0],
-            [params[1]-x[2], -1, -x[0]],
-            [x[1], x[0], -params[2]]
-        ])
+def lorentz3d_jac_dsigma(t, x, sigma, r, b):
+    jac = np.array([x[1]-x[0], 0, 0])
+    return jac
 
-    raise ValueError('x is the wrong shape in lorentz3d_jac call.')
+def lorentz3d_jac_dr(t, x, sigma, r, b):
+    jac = np.array([0, x[0], 0])
+    return jac
+
+def lorentz3d_jac_db(t, x, sigma, r, b):
+    jac = np.array([0, 0, -x[2]])
+    return jac

examples/benchmark_problems/Data_Assimilation/functions/simplified_anac.py
@@ -27,12 +27,8 @@ def simplified_anac(t, x, gamma, mu):
 
     raise ValueError('x is the wrong shape in simplified_anac call.')
 
-def simplified_anac_jac(x, params):
+def simplified_anac_jac_dgamma(t, x, gamma, mu):
+    return np.array([0, x[0]])
 
-    if len(x.shape) == 1:
-        return np.array([[
-            0, 1],
-            [params[0] + 6*params[1]*x[0]**2, 0]
-        ])
-
-    raise ValueError('x is the wrong shape in simplified_anac_jac call.')
+def simplified_anac_jac_dmu(t, x, gamma, mu):
+    return np.array([0, 2*x[0]**3])

fitbenchmarking/jacobian/analytic_jacobian.py
@@ -32,9 +32,15 @@ class Analytic(Jacobian):
         x = kwargs.get("x", self.problem.data_x)
         y = kwargs.get("y", self.problem.data_y)
         e = kwargs.get("e", self.problem.data_e)
+        
+        # flatten in case of vector function
+        e = np.ravel(e)
+        y = np.ravel(e)
+
         print(params)
         jac = self.problem.jacobian(x, params)
-        print(jac)
+        print(np.shape(jac))
+        print(jac[0:50,:])
         if self.problem.options.cost_func_type == "weighted_nlls":
             # scales each column of the Jacobian by the weights
             jac = jac / e[:, None]
@@ -67,5 +73,7 @@ class Analytic(Jacobian):
                                          params,
                                          **kwargs)
             out = 2.0 * np.matmul(J.T, rx)
+
+        print(out)
 
         return out

fitbenchmarking/parsing/fitbenchmark_parser.py
@@ -100,6 +100,14 @@ class FitbenchmarkParser(Parser):
             fitting_problem.function = self._create_ivp_function()
             fitting_problem.format = 'ivp'
 
+        # JACOBIAN
+        if software == 'ivp':
+            try:
+                fitting_problem.jacobian = self._parse_ivp_jacobian()
+            except NoJacobianError:
+                LOGGER.warning("Could not find analytic Jacobian "
+                                "information for %s problem", fitting_problem.name)
+
         # If using a multivariate function wrap the call to take a single
         # argument
         if len(data_points[0]['x'].shape) > 1:
@@ -115,6 +123,13 @@ class FitbenchmarkParser(Parser):
             def new_function(x, *p):
                 inp = all_data[x]
                 return old_function(inp, *p)
+
+            if fitting_problem.jacobian:
+                old_jacobian = fitting_problem.jacobian
+                def new_jacobian(x, p):
+                    inp = all_data[x]
+                    return old_jacobian(inp, p)
+                fitting_problem.jacobian = new_jacobian
 
             fitting_problem.function = new_function
             fitting_problem.multivariate = True
@@ -133,14 +148,6 @@ class FitbenchmarkParser(Parser):
             else:
                 fitting_problem.equation = '{} Functions'.format(
                     equation_count)
-
-        # JACOBIAN
-        if software == 'ivp':
-            try:
-                fitting_problem.jacobian = self._parse_ivp_jacobian()
-            except NoJacobianError:
-                LOGGER.warning("Could not find analytic Jacobian "
-                                "information for %s problem", fitting_problem.name)
 
         # STARTING VALUES
         if software == 'ivp':
@@ -508,9 +515,31 @@ class FitbenchmarkParser(Parser):
         path = os.path.join(os.path.dirname(self._filename), jacobian_info['module'])
         sys.path.append(os.path.dirname(path))
         module = importlib.import_module(os.path.basename(path))
-        fun = getattr(module, jacobian_info['func'])
 
-        return fun
+        jac_fun = jacobian_info['jac']
+        jac_dict = dict(j.split(":") for j in jac_fun.split(";"))
+        time_step = jacobian_info['step']
+
+        def fitFunction_jac(x, *p):
+            p = tuple(p[0])
+            if len(x.shape) == 1:
+                x = np.array([x])
+            jac = np.zeros((np.shape(x)[0],np.shape(x)[1],len(p)))
+            for i, inp in enumerate(x):
+                j = 0
+                for k,v in jac_dict.items():
+                    fun = getattr(module, v)
+                    soln = solve_ivp(fun=fun,
+                                     t_span=[0, time_step],
+                                     y0=inp,
+                                     args=p,
+                                     vectorized=False)
+                    jac[i,:,j] = soln.y[:, -1]
+                    j += 1
+            new_jac = np.reshape(jac,(np.shape(x)[0]*np.shape(x)[1],len(p)))
+            return new_jac
+
+        return fitFunction_jac
 
 
 def _parse_range(range_str):