f3b0c41ab1ed8daa490ac39a93cc4840949b442a
Robert Applin <robert.applin@stfc.ac.uk>
2021-10-18 12:34:40 +0100

    Refs #929. Add tests for file writter decorator.

--- fitbenchmarking/utils/write_files.py
+++ fitbenchmarking/utils/write_files.py
@@ -7,21 +7,13 @@ from fitbenchmarking.utils.log import get_logger
 
 LOGGER = get_logger()
 
-FILEPATH_MESSAGE = "\nERROR: \n   The provided results directory has a " \
-                   "file path which is too long. Please change the \n   " \
-                   "path to the results directory so that it is well " \
-                   "within the 260 character limit.\n"
+FILEPATH_MESSAGE = "\nERROR: \n   The file path to the results directory " \
+                   "is too long. Please change the results directory\n   " \
+                   "so that it is well within the 260 character limit.\n"
 
 UNKNOWN_MESSAGE = "\nERROR: \n   An unexpected error occurred while " \
                   "writing the output files. Please report this message " \
                   "to the FitBenchmarking team."
-
-# Tries to find a windows-specific builtin type. If on a different operating
-# system, it sets the type to None
-try:
-    WindowsError
-except NameError:
-    WindowsError = None
 
 
 def write_file(function):
@@ -40,11 +32,10 @@ def write_file(function):
     def wrapper(*args, **kwargs):
         try:
             return function(*args, **kwargs)
-        except WindowsError as ex:
-            filepath_error = ex.winerror == 206
         except FileNotFoundError as ex:
-            filepath_error = "No such file or directory" in str(ex)
+            filepath_err = "The filename or extension is too long" in \
+                           str(ex) or "No such file or directory" in str(ex)
 
-        LOGGER.error(FILEPATH_MESSAGE if filepath_error else UNKNOWN_MESSAGE)
+        LOGGER.error(FILEPATH_MESSAGE if filepath_err else UNKNOWN_MESSAGE)
 
     return wrapper
