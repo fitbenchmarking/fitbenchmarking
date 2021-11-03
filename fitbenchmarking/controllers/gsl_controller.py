168d3b9bc70e1ec888ba07a4a5f75742b061ffa1
Robert Applin <robert.applin@stfc.ac.uk>
2021-11-03 11:42:17 +0000

    Refs #957. Do not display reports which are not used.

--- fitbenchmarking/controllers/gsl_controller.py
+++ fitbenchmarking/controllers/gsl_controller.py
@@ -211,7 +211,6 @@ class GSLController(Controller):
         """
         Run problem with GSL
         """
-        # pylint: disable=c-extension-no-member
         for _ in range(self._maxits):
             status = self._solver.iterate()
             # check if the method has converged
@@ -236,7 +235,6 @@ class GSLController(Controller):
                 self.flag = 2
         else:
             self.flag = 1
-        # pylint: enable=c-extension-no-member
 
     def cleanup(self):
         """
