1592eac73cb9062e0d2f605633f38a8e43e87218
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2021-11-10 16:53:21 +0000

    add rest of the install steps

--- build/build-scripts.sh
+++ build/build-scripts.sh
@@ -61,6 +61,49 @@ cd  $CUTEST
 printf "6\n2\n6\n2\n8\n" > cutest.input
 printf "nnydn" >> cutest.input
 ./install_cutest < cutest.input
-python -m pip install pycutest
+pip install pycutest
 
 # Install mantid
+cd $HERE
+# set noninteractive to stop tzdata prompt
+export DEBIAN_FRONTEND=noniteractive
+OLD_VENV=VIRTUAL_ENV
+export VIRTUAL_ENV=""
+export OLD_PATH=$PATH
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+sudo apt-get install -y git g++ qt5-default clang-format-6.0 cmake dvipng doxygen libtbb-dev \
+  libgoogle-perftools-dev libboost-all-dev libpoco-dev libnexus-dev libhdf5-dev libhdf4-dev \
+  libjemalloc-dev libgsl-dev liboce-visualization-dev libmuparser-dev libssl-dev libjsoncpp-dev \
+  librdkafka-dev qtbase5-dev qttools5-dev qttools5-dev-tools libqt5webkit5-dev \
+  libqt5x11extras5-dev libqt5opengl5-dev libqscintilla2-qt5-dev libpython3-dev \
+  ninja-build python3-setuptools python3-sip-dev python3-pyqt5 pyqt5-dev pyqt5-dev-tools \
+  python3-qtpy python3-numpy python3-scipy python3-sphinx python3-sphinx-bootstrap-theme \
+  python3-pycifrw python3-dateutil python3-matplotlib python3-qtconsole python3-h5py \
+  python3-mock python3-psutil python3-requests python3-toml python3-yaml wget
+
+# Details from mantid website
+wget https://downloads.sourceforge.net/project/mantid/6.0/mantid-6.0.0-Source.tar.xz
+tar -xvf mantid-6.0.0-Source.tar.xz
+rm mantid*.xz
+
+sudo apt-get install -y ccache
+ccache --max-size=20G
+
+mkdir -p /opt/Mantid
+
+cd /opt/Mantid
+cmake ${HERE}/mantid-6.0.0-Source -DENABLE_MANTIDPLOT=OFF
+make
+
+export VIRTUAL_ENV=$OLD_VENV
+export PATH=$OLD_PATH
+unset OLD_PATH
+export PYTHONPATH=$PYTHONPATH:/opt/Mantid/lib:/opt/Mantid/bin
+pip install IPython six python-dateutil pyyaml h5py
+export HDF5_DISABLE_VERSION_CHECK=2
+/opt/Mantid/bin/mantidpython -m  mantid.simpleapi || echo "expected segfault on first run"
+
+cd $HERE
+
+# setup pygsl
+pip install pygsl
