90b29cf5520b93a72cf6a206ba48b8169ce29214
Tyrone Rees <tyrone.rees@stfc.ac.uk>
2021-11-10 16:15:55 +0000

    make the apt-gets sudo calls

--- build/build-scripts.sh
+++ build/build-scripts.sh
@@ -1,9 +1,9 @@
 # First, let's setup the basics
-apt-get update
-apt-get install -y \
-	python3 \
-	python3-pip \
-	python3-dev \
+sudo apt-get update
+sudo apt-get install -y \
+     python3 \
+     python3-pip \
+     python3-dev \
 	python3-venv \
 	git \
 	sudo \
