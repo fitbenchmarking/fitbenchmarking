dbd662da4ff97f0b203ef326b74001ab79523c01
joshuadkitenge <joshua.kitenge@stfc.ac.uk>
2022-10-21 11:04:41 +0100

    ceres test #217

--- fitbenchmarking/controllers/ceres_controller.py
+++ fitbenchmarking/controllers/ceres_controller.py
@@ -5,6 +5,7 @@ import sys
 import os
 from fitbenchmarking.controllers.base_controller import Controller
 from fitbenchmarking.utils.exceptions import UnknownMinimizerError
+import numpy as np 
 pyceres_location = os.environ["PYCERES_LOCATION"]
 sys.path.insert(0, pyceres_location)
 
@@ -17,35 +18,31 @@ class CeresCostFunction(PyCeres.CostFunction):
     """
     Cost function for Ceres solver
     """
-    def __init__(self):
+    def __init__(self,cf):
         # MUST BE CALLED. Initializes the Ceres::CostFunction class
         super().__init__()
+        self.fb_cf = cf
 
         # MUST BE CALLED. Sets the size of the residuals and parameters
         self.set_num_residuals(1)
         self.set_parameter_block_sizes([1])
 
-        # Used to check whether the algorithm type of the
-        # selected minimizer is incompatible with the cost function
 
-        self.invalid_algorithm_types = ['ls']
 
     # The CostFunction::Evaluate(...) virtual function implementation
 
-    # pylint: disable=no-self-use
-    def Evaluate(self, parameters, residuals, jacobians):
+    def Evaluate(self,parameters, residuals, jacobians):
         """
         Evaluate for Ceres solver
         """
         x = parameters[0][0]
 
-        residuals[0] = 10 - x
+        residuals[0] = self.fb_cf.eval_cost(x)
+        if jacobians is not None:
+            jacobians[0][0] = self.fb_cf.jac_cost(x)
 
-        if jacobians is not None:  # check for Null
-            jacobians[0][0] = -1
 
         return True
-    # pylint: enable=no-self-use
 
 
 class CeresController(Controller):
@@ -107,13 +104,13 @@ class CeresController(Controller):
         self.ceres_problem = PyCeres.Problem()
         self.ceres_options = PyCeres.SolverOptions()
         self.ceres_summary = PyCeres.Summary()
-        self.ceres_cost_func = CeresCostFunction()
+        self.ceres_cost_func = CeresCostFunction(self.cost_func)
 
     def setup(self):
         """
         Setup problem ready to be run with Ceres solver
         """
-        self.result = self.initial_params
+        self.result = np.array(self.initial_params)
         print("intial results", self.result)
 
         self.ceres_problem.AddResidualBlock(self.ceres_cost_func, None,
@@ -161,9 +158,8 @@ class CeresController(Controller):
         PyCeres.Solve(self.ceres_options, self.ceres_problem,
                       self.ceres_summary)
 
-        self.result = self.initial_params
 
-        self._status = 0 if self.ceres_summary.IsSolutionUsable() is True \
+        self._status = 0 if self.ceres_summary.IsSolutionUsable()  \
             else 1
 
     def cleanup(self):
@@ -175,5 +171,6 @@ class CeresController(Controller):
             self.flag = 0
         else:
             self.flag = 2
-
+        print(self.ceres_summary.message)
         self.final_params = self.result
+        print("final results", self.result)
