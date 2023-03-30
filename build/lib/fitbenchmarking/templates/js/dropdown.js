21c01846e7175e169eaef6f46737c79576d3a1fb
RabiyaF <rabiya.farooq.226@gmail.com>
2023-03-30 23:33:15 +0100

    reverted

--- build/lib/fitbenchmarking/templates/js/dropdown.js
+++
@@ -1,9 +0,0 @@
-function show_dropdown(dropdown_id) {
-    var checkList = document.getElementById(dropdown_id);
-
-    if (checkList.classList.contains('visible')) {
-        checkList.classList.remove('visible');
-    } else {
-        checkList.classList.add('visible');
-    }
-}
