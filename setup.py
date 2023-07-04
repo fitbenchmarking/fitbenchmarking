133d59d3f539ae13ed54eeabe17a9ad723c95eb8
Andrew Lister <48282025+AndrewLister-STFC@users.noreply.github.com>
2023-07-04 15:49:54 +0100

    Pin codecarbon

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
                       'pandas>=1.3',
                       'jinja2',
                       'configparser',
-                      'codecarbon'],
+                      'codecarbon<2.2.4'],
     extras_require={'DFO': ['DFO-LS', 'dfogn'],
                     'SAS': ['sasmodels',
                             'tinycc;platform_system==\'Windows\''],
