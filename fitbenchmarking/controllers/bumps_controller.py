53a2ceb9dea75c4e8305d3dbf1582942c6fd54e7
Jess Farmer <jessica.farmer@stfc.ac.uk>
2024-02-06 10:24:43 +0000

    match MCMC chain size to paramonte default

--- fitbenchmarking/controllers/bumps_controller.py
+++ fitbenchmarking/controllers/bumps_controller.py
@@ -150,7 +150,8 @@ class BumpsController(Controller):
         """
         result = bumpsFit(self._fit_problem,
                           method=self._minimizer,
-                          abort_test=self._check_timer_abort_test)
+                          abort_test=self._check_timer_abort_test,
+                          samples=100000) # add number of samples for dream
 
         self._bumps_result = result
         self._status = self._bumps_result.status
