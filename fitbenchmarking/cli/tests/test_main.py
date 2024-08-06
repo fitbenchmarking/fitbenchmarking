e90aebddcdd972b15aa8a18c251e5c63a9320607
Andrew Lister <andrew.lister@stfc.ac.uk>
2024-08-06 17:37:41 +0100

    Add test for valid cp after failed run

--- fitbenchmarking/cli/tests/test_main.py
+++ fitbenchmarking/cli/tests/test_main.py
@@ -1,18 +1,21 @@
 """
 This file contains unit tests for the main CLI script
 """
+import inspect
+import os
+from json import load
+from tempfile import TemporaryDirectory
 from unittest import TestCase
 from unittest.mock import patch
-import os
-import inspect
 
-from fitbenchmarking.cli import main
 from fitbenchmarking import test_files
-from fitbenchmarking.cost_func.nlls_cost_func import NLLSCostFunc
+from fitbenchmarking.cli import main
 from fitbenchmarking.controllers.scipy_controller import ScipyController
-from fitbenchmarking.utils import fitbm_result, exceptions
-from fitbenchmarking.utils.options import Options
+from fitbenchmarking.cost_func.nlls_cost_func import NLLSCostFunc
 from fitbenchmarking.parsing.parser_factory import parse_problem_file
+from fitbenchmarking.utils import exceptions, fitbm_result
+from fitbenchmarking.utils.misc import get_problem_files
+from fitbenchmarking.utils.options import Options
 
 
 def make_controller(file_name='cubic.dat', minimizers=None):
@@ -66,7 +69,7 @@ class TestMain(TestCase):
 
         with self.assertRaises(exceptions.NoResultsError):
             main.run(['examples/benchmark_problems/simple_tests'],
-                     os.path.dirname(__file__), debug=True)
+                     debug=True)
 
     @patch('fitbenchmarking.cli.main.Fit.benchmark')
     def test_all_dummy_results_produced(self, benchmark):
@@ -77,4 +80,30 @@ class TestMain(TestCase):
 
         with self.assertRaises(exceptions.NoResultsError):
             main.run(['examples/benchmark_problems/simple_tests'],
-                     os.path.dirname(__file__), debug=True)
+                     debug=True)
+
+    @patch('fitbenchmarking.cli.main.save_results')
+    @patch('fitbenchmarking.utils.misc.get_problem_files')
+    def test_checkpoint_file_on_fail(self, get_problems, save_results):
+        """
+        Checks that the checkpoint file is valid json if there's a crash.
+        """
+        get_problems.side_effect = lambda path: [get_problem_files(path)[0]]
+        save_results.side_effect = RuntimeError(
+            "Exception raised during save...")
+
+        with TemporaryDirectory() as results_dir:
+            with self.assertRaises(RuntimeError):
+                main.run(['examples/benchmark_problems/simple_tests'],
+                         additional_options={'scipy_ls': ['lm-scipy'],
+                                             'software': ['scipy_ls'],
+                                             'num_runs': 1,
+                                             'results_dir': results_dir},
+                         debug=True)
+
+            with open(f'{results_dir}/checkpoint.json', encoding='utf8') as f:
+                # This will fail if the json is invalid
+                contents = load(f)
+
+        # Check that it's not empty
+        self.assertTrue(contents)
