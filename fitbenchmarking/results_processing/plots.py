294b24852bedcd25352f73c93155de1d5716db11
Andrew Lister <andrew.lister@stfc.ac.uk>
2025-01-06 14:55:37 +0000

    Tidy up rebase errors

--- fitbenchmarking/results_processing/plots.py
+++ fitbenchmarking/results_processing/plots.py
@@ -254,8 +254,6 @@ class Plot:
                 )
 
         fig.update_layout(showlegend=False)
-        fig.update_xaxes(exponentformat="power")
-        fig.update_yaxes(exponentformat="power")
 
         html_fname = (
             f"{result.sanitised_min_name(True)}_posterior_"
