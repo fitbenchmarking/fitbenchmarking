5163d2c1a5b1c19b10f5942531bf8a69dbac48b0
RabiyaF <47083562+RabiyaF@users.noreply.github.com>
2025-03-11 15:48:43 +0000

    added tests for _parse_function

--- fitbenchmarking/parsing/tests/test_mantid_parser.py
+++ fitbenchmarking/parsing/tests/test_mantid_parser.py
@@ -23,13 +23,7 @@ class TestMantidDevParser(TestCase):
         """
         Set up resources before each test case.
         """
-        path = (
-            Path("examples")
-            / "benchmark_problems"
-            / "Mantid_System_Test_Data"
-            / "osi97935"
-            / "spectrum_0.txt"
-        )
+        path = Path(__file__).parent / "mantiddev" / "basic.txt"
         mantid_parser_cls = ParserFactory.create_parser(path)
         with patch.object(mantid_parser_cls, "__init__", lambda a, b, c: None):
             self.parser = mantid_parser_cls("test_file.txt", {"parse"})
@@ -64,3 +58,9 @@ class TestMantidDevParser(TestCase):
         """
         self.parser._entries = entries
         assert self.parser._parse_ties() == expected
+
+    def test_parse_function(self):
+        """
+        Verifies the output of _parse_function() method.
+        """
+        assert self.parser._parse_function() == []
