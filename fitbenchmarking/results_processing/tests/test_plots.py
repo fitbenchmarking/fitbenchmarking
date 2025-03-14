af23a2ab631abf88364434daf8df89a0f3e366ca
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2025-03-14 14:56:43 +0000

    Update tests

--- fitbenchmarking/results_processing/tests/test_plots.py
+++ fitbenchmarking/results_processing/tests/test_plots.py
@@ -177,10 +177,12 @@ class PlotTests(unittest.TestCase):
             self.assertTrue(os.path.exists(path))
             self.assertEqual(find_error_bar_count(path), 2)
 
-    @mock.patch("fitbenchmarking.results_processing.plots.Plot.plotly_spinw")
-    def test_plotly_spinw_called(self, spinw_plot):
+    @mock.patch(
+        "fitbenchmarking.results_processing.plots.Plot._check_spinw_data_len"
+    )
+    def test__check_spinw_data_len_called(self, check_data_len):
         """
-        Test that plotly_fit calls plotly_spinw
+        Test that plotly_fit and plots_residuals call _check_spinw_data_len
         when spinw_plot_info is set.
         """
         self.plot.result.spinw_plot_info = {
@@ -190,47 +192,47 @@ class PlotTests(unittest.TestCase):
             "ebin_cens": [0.9, 1.6, 1.1],
         }
 
-        self.plot.plot_initial_guess(self.df[("Fake_Test_Data", "prob_1")])
-        spinw_plot.assert_called()
-
         self.plot.plotly_fit(self.df[("Fake_Test_Data", "prob_1")])
-        spinw_plot.assert_called()
+        check_data_len.assert_called()
 
-    @mock.patch("fitbenchmarking.results_processing.plots.Plot.plotly_spinw")
-    def test_plotly_spinw_fit_not_called(self, spinw_plot):
-        """
-        Test that plotly_fit doesn't call plotly_spinw
-        when spinw_plot_info is not set.
-        """
-        self.plot.plot_initial_guess(self.df[("Fake_Test_Data", "prob_1")])
-        spinw_plot.assert_not_called()
+        self.plot.plot_residuals(
+            categories=self.fr,
+            title="",
+            options=self.opts,
+            figures_dir=self.figures_dir,
+        )
+        check_data_len.assert_called()
 
+    @mock.patch(
+        "fitbenchmarking.results_processing.plots.Plot._check_spinw_data_len"
+    )
+    def test__check_spinw_data_len_not_called(self, check_data_len):
+        """
+        Test that plotly_fit and plots_residuals don't call
+        _check_spinw_data_len when spinw_plot_info is not set.
+        """
         self.plot.plotly_fit(self.df[("Fake_Test_Data", "prob_1")])
-        spinw_plot.assert_not_called()
+        check_data_len.assert_not_called()
 
-    def test_plotly_spinw(self):
+        self.plot.plot_residuals(
+            categories=self.fr,
+            title="",
+            options=self.opts,
+            figures_dir=self.figures_dir,
+        )
+        check_data_len.assert_not_called()
+
+    def test__check_spinw_data_len_raises_error(self):
         """
-        Test that plotly_spinw creates correct number of subplots
-        and raises error if data lengths are unexpected
+        Test _check_spinw_data_len raises error if data lengths are
+        unexpected
         """
-        self.plot.result.spinw_plot_info = {
-            "plot_type": "1d_cuts",
-            "n_cuts": 3,
-            "q_cens": [0.5, 1, 1.5],
-            "ebin_cens": [0.9],
-        }
         data = self.df[("Fake_Test_Data", "prob_1")]
         y_best = data["y"][data["best"]]
-
-        fig = self.plot.plotly_spinw(data, "m10_[s1]_jj0", y_best)
-        _, cols = fig._get_subplot_rows_columns()
-
-        assert len(cols) == self.plot.result.spinw_plot_info["n_cuts"]
-
-        self.plot.result.spinw_plot_info["ebin_cens"] = [2, 4]
+        x_best = 10 * data["x"][data["best"]].to_list()
 
         with self.assertRaises(PlottingError):
-            self.plot.plotly_spinw(data, "m10_[s1]_jj0", y_best)
+            self.plot._check_spinw_data_len(y_best, x_best)
 
     def test_plot_posteriors_create_files(self):
         """
@@ -304,10 +306,10 @@ class PlotTests(unittest.TestCase):
         path = os.path.join(self.figures_dir, file_name)
         self.assertTrue(os.path.exists(path))
 
-    def test_create_empty_residuals_plot_spinw(self):
+    def test_create_empty_residuals_plots(self):
         """
         Test that create_empty_residuals_plot_spinw creates correct
-        number of subplots and raises error if data lengths are unexpected
+        number of subplots
         """
         result = next(iter(self.fr.values()))[0]
         result.spinw_plot_info = {
@@ -316,10 +318,11 @@ class PlotTests(unittest.TestCase):
             "q_cens": [0.5, 1, 1.5],
             "ebin_cens": [0.9],
         }
+        titles = 3 * ["Q_value"]
 
-        fig = self.plot._create_empty_residuals_plot_spinw(
+        fig = self.plot._create_empty_residuals_plots(
             categories=self.fr,
-            n_plots_per_row=result.spinw_plot_info["n_cuts"],
+            subplot_titles=titles,
         )
         rows, cols = fig._get_subplot_rows_columns()
 
@@ -327,12 +330,6 @@ class PlotTests(unittest.TestCase):
         assert len(cols) == result.spinw_plot_info["n_cuts"]
 
         result.spinw_plot_info["ebin_cens"] = [2, 4]
-
-        with self.assertRaises(PlottingError):
-            self.plot._create_empty_residuals_plot_spinw(
-                categories=self.fr,
-                n_plots_per_row=result.spinw_plot_info["n_cuts"],
-            )
 
 
 if __name__ == "__main__":
