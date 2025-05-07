bc5291cbd54e63a64e557bb4d156a3d8daf89622
Jessica Huntley <79837359+jess-farmer@users.noreply.github.com>
2025-05-07 14:48:05 +0100

    remove columns that are entirely nans
    Co-authored-by: Letizia97 <letizia.protopapa@stfc.ac.uk>

--- examples/benchmark_problems/SpinW_powder_data_2d/m_scripts/wye_functions/fb_wye_tri_AFM.m
+++ examples/benchmark_problems/SpinW_powder_data_2d/m_scripts/wye_functions/fb_wye_tri_AFM.m
@@ -4,7 +4,13 @@ function [w, y, e, msk] = fb_wye_tri_AFM(datafile, path)
 w = load(datafile).data;
 msk = [];
 
-% remove all nan entries
+% remove columns that are entirely nans
+entirely_NaN_cols_y = all(isnan(w.y), 1);
+w.y(:, entirely_NaN_cols_y) = [] 
+entirely_NaN_cols_e = all(isnan(w.e), 1);
+w.e(:, entirely_NaN_cols_e) = [] 
+
+% remove all nan entries - rows 
 NaN_rows_y = find(any(isnan(w.y),2));
 w.y = w.y(sum(isnan(w.y),2)==0,:);
 NaN_rows_e = find(any(isnan(w.e),2));
