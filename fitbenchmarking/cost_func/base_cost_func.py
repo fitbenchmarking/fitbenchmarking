8e90a778b0e6e70583bb5be83a1bae14aa553e75
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2025-07-22 10:17:45 +0100

    Remove commented lines

--- fitbenchmarking/cost_func/base_cost_func.py
+++ fitbenchmarking/cost_func/base_cost_func.py
@@ -158,22 +158,7 @@ class CostFunc:
             data[mask] = np.interp(
                 np.flatnonzero(mask), np.flatnonzero(~mask), data[~mask]
             )
-            # inds = np.where(np.isnan(result))[0]
-            # for ind in inds:
-            #     if ind > 0 and ind < np.shape(result)[0]-1:
-            #         result[ind] = np.mean([result[ind-1],result[ind+1]])
-            #     elif ind == 0:
-            #         result[ind] = result[ind+1]
-            #     elif ind == np.shape(result)[0]-1:
-            #         result[ind] = result[ind-1]
-
         elif len(np.shape(data)) == 2:
-            #     inds = np.where(np.isnan(result))
-            #     x_inds = inds[0]
-            #     y_inds = inds[1]
-            #     for k in np.arange(len(x_inds)):
-            #         result[x_inds[k], y_inds[k]] = 0
-
             mask = np.where(~np.isnan(data))
             interp = NearestNDInterpolator(np.transpose(mask), data[mask])
             data = interp(*np.indices(data.shape))
