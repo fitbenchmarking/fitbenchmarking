6cbaaf75b51fe9109023c003d29eb2d625e95c31
Andrew Sajo <Andrew.Sajo@amd.com>
2025-07-23 14:39:47 +0000

    Pretty format

--- fitbenchmarking/controllers/aoclda_controller.py
+++ fitbenchmarking/controllers/aoclda_controller.py
@@ -25,7 +25,7 @@ https://github.com/amd/aocl-data-analytics
 """
 
 import numpy as np
-import copy # deepcopy
+import copy  # deepcopy
 from aoclda.nonlinear_model import nlls
 
 import traceback
@@ -34,6 +34,7 @@ import traceback
 
 from fitbenchmarking.controllers.base_controller import Controller
 from fitbenchmarking.utils.exceptions import UnknownMinimizerError
+
 
 class AOCLDAController(Controller):
     """
@@ -150,7 +151,7 @@ class AOCLDAController(Controller):
         """
 
         # TODO jac_res returns a list of 1D numpy arrays
-        J[:]  = data.cost_func.jac_res(x)
+        J[:] = data.cost_func.jac_res(x)
         return 0
 
     def hes_eval(_, x, r, Hr, data) -> int:
@@ -187,37 +188,37 @@ class AOCLDAController(Controller):
 
         # Use bytestrings explicitly as python 3 defaults to unicode.
         if self.minimizer == "gn":
-            model = b"gauss-newton"             # self._options[b"model"] = 1
-            method = b"galahad"                 # self._options[b"nlls_method"] = 4
-            glob_strategy = b"tr"               # self._options[b"type_of_method"] = 1
+            model = b"gauss-newton"  # self._options[b"model"] = 1
+            method = b"galahad"  # self._options[b"nlls_method"] = 4
+            glob_strategy = b"tr"  # self._options[b"type_of_method"] = 1
         elif self.minimizer == "gn_reg":
-            model = b"gauss-newton"             # self._options[b"model"] = 1
-            method = b"galahad"                 # self._options[b"nlls_method"] = 4
-            glob_strategy = b"reg"              # self._options[b"type_of_method"] = 2
+            model = b"gauss-newton"  # self._options[b"model"] = 1
+            method = b"galahad"  # self._options[b"nlls_method"] = 4
+            glob_strategy = b"reg"  # self._options[b"type_of_method"] = 2
         elif self.minimizer == "hybrid":
-            model = b"hybrid"                   # self._options[b"model"] = 3
-            method = b"galahad"                 # self._options[b"nlls_method"] = 4
-            glob_strategy = b"tr"               # self._options[b"type_of_method"] = 1
+            model = b"hybrid"  # self._options[b"model"] = 3
+            method = b"galahad"  # self._options[b"nlls_method"] = 4
+            glob_strategy = b"tr"  # self._options[b"type_of_method"] = 1
         elif self.minimizer == "hybrid_reg":
-            model = b"hybrid"                   # self._options[b"model"] = 3
-            method = b"galahad"                 # self._options[b"nlls_method"] = 4
-            glob_strategy = b"reg"              # self._options[b"type_of_method"] = 2
+            model = b"hybrid"  # self._options[b"model"] = 3
+            method = b"galahad"  # self._options[b"nlls_method"] = 4
+            glob_strategy = b"reg"  # self._options[b"type_of_method"] = 2
         elif self.minimizer == "newton":
-            model = b"quasi-newton"             # self._options[b"model"] = 2
-            method = b"galahad"                 # self._options[b"nlls_method"] = 4
-            glob_strategy = b"tr"               # self._options[b"type_of_method"] = 1
+            model = b"quasi-newton"  # self._options[b"model"] = 2
+            method = b"galahad"  # self._options[b"nlls_method"] = 4
+            glob_strategy = b"tr"  # self._options[b"type_of_method"] = 1
         elif self.minimizer == "newton_reg":
-            model = b"quasi-newton"             # self._options[b"model"] = 2
-            method = b"galahad"                 # self._options[b"nlls_method"] = 4
-            glob_strategy = b"reg"              # self._options[b"type_of_method"] = 2
+            model = b"quasi-newton"  # self._options[b"model"] = 2
+            method = b"galahad"  # self._options[b"nlls_method"] = 4
+            glob_strategy = b"reg"  # self._options[b"type_of_method"] = 2
         elif self.minimizer == "newton-tensor":
-            model = b"tensor-newton"            # self._options[b"model"] = 4
-            method = b"galahad"                 # self._options[b"nlls_method"] = 4
-            glob_strategy = b"tr"               # self._options[b"type_of_method"] = 1
+            model = b"tensor-newton"  # self._options[b"model"] = 4
+            method = b"galahad"  # self._options[b"nlls_method"] = 4
+            glob_strategy = b"tr"  # self._options[b"type_of_method"] = 1
         elif self.minimizer == "newton-tensor_reg":
-            model = b"tensor-newton"            # self._options[b"model"] = 4
-            method = b"galahad"                 # self._options[b"nlls_method"] = 4
-            glob_strategy = b"reg"              # self._options[b"type_of_method"] = 2
+            model = b"tensor-newton"  # self._options[b"model"] = 4
+            method = b"galahad"  # self._options[b"nlls_method"] = 4
+            glob_strategy = b"reg"  # self._options[b"type_of_method"] = 2
         else:
             raise UnknownMinimizerError(
                 f"No {self.minimizer} minimizer for AOCLDA"
@@ -230,56 +231,68 @@ class AOCLDAController(Controller):
         if self.value_ranges is not None:
             self._lb, self._ub = zip(*self.value_ranges)
 
-        self._handle = nlls(n_coef=n_coef, n_res=n_res, weights=None,
-                            lower_bounds = self._lb, upper_bounds = self._ub, order='c',
-                            model = model, method = method, glob_strategy = glob_strategy,
-                            verbose=self.VERBOSE)
+        self._handle = nlls(
+            n_coef=n_coef,
+            n_res=n_res,
+            weights=None,
+            lower_bounds=self._lb,
+            upper_bounds=self._ub,
+            order="c",
+            model=model,
+            method=method,
+            glob_strategy=glob_strategy,
+            verbose=self.VERBOSE,
+        )
 
     def fit(self):
         """
         Run problem with AOCLDA.
         """
 
-        maxit = 500 # Same as RALFit
-        ftol = 1e-8     # TODO how to pass this option?
-        abs_ftol = 1e-8 # TODO how to pass this option?
-        gtol = 1e-8     # TODO how to pass this option?
-        abs_gtol = 1e-5 # TODO how to pass this option?
-        xtol = 2.22e-16 # TODO how to pass this option?
+        maxit = 500  # Same as RALFit
+        ftol = 1e-8  # TODO how to pass this option?
+        abs_ftol = 1e-8  # TODO how to pass this option?
+        gtol = 1e-8  # TODO how to pass this option?
+        abs_gtol = 1e-5  # TODO how to pass this option?
+        xtol = 2.22e-16  # TODO how to pass this option?
 
         use_fd = False  # TODO how to pass this option?
         fd_step = 1e-7  # TODO how to pass this option?
         fd_ttol = 1e-4  # TODO how to pass this option?
 
         if self.cost_func.jacobian and not use_fd:
-            jac = self.jac_eval # AOCLDA wrapper to self.cost_func.jac_res
+            jac = self.jac_eval  # AOCLDA wrapper to self.cost_func.jac_res
         else:
-            jac = None # Request finite-differences
+            jac = None  # Request finite-differences
 
         if self.cost_func.hessian:
-            hes = self.hes_eval # AOCLDA wrapper to self.cost_func.hessian
+            hes = self.hes_eval  # AOCLDA wrapper to self.cost_func.hessian
         else:
             hes = None
 
-        self._x = np.array(self.initial_params) # x has to be np.array and is overwritten!
+        self._x = np.array(
+            self.initial_params
+        )  # x has to be np.array and is overwritten!
 
         try:
-            self._handle.fit(x=self._x,
-                            fun=self.eval_r,
-                            jac=jac,
-                            hes=hes,
-                            hep=None,
-                            data=self,
-                            maxit=maxit)
-                            #ftol=ftol,
-                            #abs_ftol=abs_ftol,
-                            #gtol=gtol,
-                            #abs_gtol,
-                            #xtol=xtol,
-                            #fd_step=fd_step,
-                            #fd_ttol=fd_ttol)
+            self._handle.fit(
+                x=self._x,
+                fun=self.eval_r,
+                jac=jac,
+                hes=hes,
+                hep=None,
+                data=self,
+                maxit=maxit,
+            )
+        # ftol=ftol,
+        # abs_ftol=abs_ftol,
+        # gtol=gtol,
+        # abs_gtol,
+        # xtol=xtol,
+        # fd_step=fd_step,
+        # fd_ttol=fd_ttol)
 
-        except RuntimeWarning: #FIXME this is not caught
+        except RuntimeWarning:  # FIXME this is not caught
             #    2: Software run but didn’t converge to solution
             #    6: Solver has exceeded maximum allowed runtime
             self._status = 2
@@ -287,16 +300,16 @@ class AOCLDAController(Controller):
             if self.VERBOSE > 0:
                 print(e)
                 print(traceback.format_exc())
-            self._status = 3 # Software raised an exception
+            self._status = 3  # Software raised an exception
         else:
             #    0: Successfully converged
             #    1: Software reported maximum number of iterations exceeded
             #    5: Solution doesn’t respect parameter bounds
             ok = True
             if self._lb is not None:
-                ok = ok and all(self._lb<=x)
+                ok = ok and all(self._lb <= x)
             if self._ub is not None:
-                ok = ok and all(self._ub>=x)
+                ok = ok and all(self._ub >= x)
 
             if maxit <= self._handle.n_iter:
                 self._status = 1
@@ -311,13 +324,19 @@ class AOCLDAController(Controller):
         will be read from.
         """
 
-        if self._status==0 or self._status == 1 or self._status==2 or self._status==5:
+        if (
+            self._status == 0
+            or self._status == 1
+            or self._status == 2
+            or self._status == 5
+        ):
             self.iteration_count = self._handle.n_iter
-            self.func_evals = self._handle.n_eval['f'] # 'j', 'h', 'hp', 'fd_f' as well available
+            self.func_evals = self._handle.n_eval[
+                "f"
+            ]  # 'j', 'h', 'hp', 'fd_f' as well available
 
         # Final Params: The final values for the params from the minimizer
         self.final_params = self._x.tolist()
 
         # Flag: error handling flag
         self.flag = self._status
-
