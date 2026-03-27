b9c8568ab9535598b82784ab26cf22bdc5c2730a
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2026-03-27 18:04:01 +0000

    Improve comments

--- fitbenchmarking/controllers/sasfit_controller.py
+++ fitbenchmarking/controllers/sasfit_controller.py
@@ -123,34 +123,35 @@ class SASFitController(Controller):
         # ---- Convert numpy arrays to c float* ----
         self.x_ptr = self.data_x.ctypes.data_as(POINTER(c_float))
         self.y_ptr = self.data_y.ctypes.data_as(POINTER(c_float))
-        self.sig_ptr = __.ctypes.data_as(
+        self.sig_ptr = self.data_e.ctypes.data_as(
             POINTER(c_float)
-        )  # FIXME: This is the standard deviation associated with  the data
+        )  # FIXME: standard deviation -- do we have this? Is it data_e?
         self.yfit_ptr = yfit_np.ctypes.data_as(POINTER(c_float))
 
         # ---- Other inputs for fitting ----
-        # FIXME: below params are required to re-run the fit, but no
-        # clue whether we can read them in from initial_params.
+        # FIXME: below params are required to run the fit, but 
+        # can we read them in from initial_params?
         # ma and mfit are n parameters and n fitted
         # defining some dummy here for simplicity
         self.ma = c_int(
             self.initial_params["n_params"]
-        )  # n params  # FIXME: determine using len
-        self.mfit = c_int(self.initial_params["n_fitted_params"])  # n fitted
-        self.alamda = c_float(
-            self.initial_params["alamda"]
-        )  # Levenberg–Marquardt control parameter
-        self.error_type = c_int(
-            self.initial_params["error_type"]
-        )  # this is 0-4 for linera, log, sqrt, poisson-like
+        )  # n total params (fitted and not)  # FIXME: determine using len ?
+        self.mfit = c_int(self.initial_params["n_fitted_params"])  
+        self.alamda = c_float(-1)  # LM control parameter, just needs to be < 0
 
-        # ---- LM parameter arrays ----
-        self.a_arr = (c_float * self.ma)(
-            *[1.0, 1.0]
-        )  # initial guess        # FIXME: Substitute with intial params
-        self.da_arr = (c_float * self.ma)()
-        self.atry_arr = (c_float * self.ma)()
-        self.beta_arr = (c_float * self.mfit)()
+        
+        # error_type controls how the data (y) and model outputs (ymod) are 
+        # transformed before computing residuals and χ²
+        # It's 0-4 for linear, log, sqrt, poisson-like
+        self.error_type = c_int(
+            self.initial_params["error_type"]  # FIXME: do we ask the user to give this?
+        )  
+
+        # ---- LM parameter arrays ----      
+        self.a_arr = (c_float * self.ma)(*self.initial_params)  # e.g. (c_float * self.ma)(*[1.0, 1.0])
+        self.da_arr = (c_float * self.ma)()  # proposed parameter changes
+        self.atry_arr = (c_float * self.ma)()  # the trial parameter vector
+        self.beta_arr = (c_float * self.mfit)()  # the gradient vector
         self.lista_arr = (c_int * self.mfit)(*list(range(self.mfit)))
 
         # ---- Matrices: alpha, covar (mfit x mfit), oneda (mfit x 1) ----
@@ -165,23 +166,23 @@ class SASFitController(Controller):
         self.lib.SASFITmrqmin(
             self.x_ptr,  # x values
             self.y_ptr,  # measured y
-            self.sig_ptr,  # signma values
+            self.sig_ptr,  # standard dev
             self.yfit_ptr,  # model prediction
-            c_int(self.ndata),
-            self.a_arr,
+            c_int(self.ndata), # n data
+            self.a_arr,  # initial params
             self.atry_arr,
             self.da_arr,
             self.beta_arr,
-            c_int(self.ma),
-            self.lista_arr,
-            c_int(self.mfit),
-            self.covar_mat,  # maybe should be byref
-            self.alpha_mat,  # maybe should be byref
-            self.oneda_mat,
+            c_int(self.ma), # n total params
+            self.lista_arr, # parameter–index mapping array to keep track of which parameters are being fitted
+            c_int(self.mfit), # n fitted params
+            self.covar_mat,  # working space array
+            self.alpha_mat,  # working space array
+            self.oneda_mat,  # working space array
             byref(self.chisq),  # Pointer to current chi‑square value
-            self.cost_func.eval_cost,  # FIXME: this should have FUNC_T signature
+            _________,  # FIXME: this should be the model function , e.g., the form factor
             byref(self.alamda),  # Levenberg–Marquardt control parameter
-            c_int(self.error_type),
+            c_int(self.error_type), # 0-4
             byref(c_bool(False)),  # Set to TRUE if anything goes wrong
         )
 
@@ -198,3 +199,6 @@ class SASFitController(Controller):
         MatArray = POINTER(c_float) * rows
         mat = MatArray(*[row_buffers[r] for r in range(rows)])
         return mat, row_buffers
+    
+    def cleanup(self):
+        return
