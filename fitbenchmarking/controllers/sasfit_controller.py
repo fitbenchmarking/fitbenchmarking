c8f5cd47ae8397041dbe14f8b7633d582e7ee89b
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2026-04-01 16:21:30 +0100

    Add sasfit controller to the options

--- fitbenchmarking/controllers/sasfit_controller.py
+++ fitbenchmarking/controllers/sasfit_controller.py
@@ -2,24 +2,40 @@
 Implements a controller for SASFit
 """
 
-import numpy as np
 import ctypes as ct
 from ctypes import (
-    c_int,
-    c_float,
-    c_bool,
-    POINTER,
     CFUNCTYPE,
+    POINTER,
     byref,
+    c_bool,
+    c_float,
+    c_int,
 )
 
+import numpy as np
+
 from fitbenchmarking.controllers.base_controller import Controller
+
+FUNCS_T = CFUNCTYPE(
+    c_float,
+    c_float,  # x_i
+    POINTER(c_float),  # a
+    POINTER(c_float),  # ymod
+    POINTER(c_float),  # dyda
+    # c_int,                  # error_type
+    # POINTER(c_bool)         # error
+)
 
 
 class SASFitController(Controller):
     """
     Controller for the SASFit fitting method.
     """
+
+    controller_name = "sasfit"
+    algorithm_check = {
+        "all": ["lm-sasfit"],
+    }
 
     def __init__(self, cost_func):
         """
@@ -36,16 +52,8 @@ class SASFitController(Controller):
         Setup problem ready to be run
         """
         # Load library
-        self.lib = ct.CDLL("sasfit_controller/liblmfit.so")
-
-        FUNCS_T = CFUNCTYPE(
-            c_float,
-            c_float,  # x_i
-            POINTER(c_float),  # a
-            POINTER(c_float),  # ymod
-            POINTER(c_float),  # dyda
-            # c_int,                  # error_type
-            # POINTER(c_bool)         # error
+        self.lib = ct.CDLL(
+            "/home/letizia/fitbenchmarking_new/fitbenchmarking/fitbenchmarking/controllers/sasfit_controller/liblmfit.so"
         )
 
         # ---- Bind the C functions ----
@@ -110,6 +118,9 @@ class SASFitController(Controller):
         ]
         self.lib.SASFITcovsrt.restype = None
 
+        self.n_params = len(self.initial_params)
+        self.n_fitted_params = len(self.initial_params)
+
         ############################################################
         # Setup the problem
         ############################################################
@@ -121,38 +132,38 @@ class SASFitController(Controller):
         self.chisq = c_float(0.0)
 
         # ---- Convert numpy arrays to c float* ----
-        self.x_ptr = self.data_x.ctypes.data_as(POINTER(c_float))
-        self.y_ptr = self.data_y.ctypes.data_as(POINTER(c_float))
-        self.sig_ptr = self.data_e.ctypes.data_as(
+        self.data_x_np = np.asarray(self.data_x, dtype=np.float32, order="C")
+        self.data_y_np = np.asarray(self.data_y, dtype=np.float32, order="C")
+        self.data_e_np = np.asarray(self.data_e, dtype=np.float32, order="C")
+
+        self.x_ptr = self.data_x_np.ctypes.data_as(POINTER(c_float))
+        self.y_ptr = self.data_y_np.ctypes.data_as(POINTER(c_float))
+        self.sig_ptr = self.data_e_np.ctypes.data_as(
             POINTER(c_float)
-        )  # FIXME: standard deviation -- do we have this? Is it data_e?
+        )  # FIXME: standard deviation
         self.yfit_ptr = yfit_np.ctypes.data_as(POINTER(c_float))
 
         # ---- Other inputs for fitting ----
-        # FIXME: below params are required to run the fit, but 
-        # can we read them in from initial_params?
-        # ma and mfit are n parameters and n fitted
-        # defining some dummy here for simplicity
-        self.ma = c_int(
-            self.initial_params["n_params"]
-        )  # n total params (fitted and not)  # FIXME: determine using len ?
-        self.mfit = c_int(self.initial_params["n_fitted_params"])  
-        self.alamda = c_float(-1)  # LM control parameter, just needs to be < 0
+        # FIXME: below params are required to run the fit
+        self.ma = self.n_params  # n total params (fitted and not)
+        self.mfit = self.n_fitted_params
+        self.alamda = c_float(
+            -1.0
+        )  # LM control parameter, just needs to be < 0
 
-        
-        # error_type controls how the data (y) and model outputs (ymod) are 
+        # error_type controls how the data (y) and model outputs (ymod) are
         # transformed before computing residuals and χ²
         # It's 0-4 for linear, log, sqrt, poisson-like
-        self.error_type = c_int(
-            self.initial_params["error_type"]  # FIXME: do we ask the user to give this?
-        )  
+        self.error_type = 0  # FIXME: should the user give this?
 
-        # ---- LM parameter arrays ----      
-        self.a_arr = (c_float * self.ma)(*self.initial_params)  # e.g. (c_float * self.ma)(*[1.0, 1.0])
+        # ---- LM parameter arrays ----
+        self.a_arr = (c_float * self.ma)(
+            *self.initial_params
+        )  # e.g. (c_float * self.ma)(*[1.0, 1.0])
         self.da_arr = (c_float * self.ma)()  # proposed parameter changes
         self.atry_arr = (c_float * self.ma)()  # the trial parameter vector
         self.beta_arr = (c_float * self.mfit)()  # the gradient vector
-        self.lista_arr = (c_int * self.mfit)(*list(range(self.mfit)))
+        self.lista_arr = (c_int * self.mfit)(*range(self.mfit))
 
         # ---- Matrices: alpha, covar (mfit x mfit), oneda (mfit x 1) ----
         self.alpha_mat, _ = self.make_matrix_float(self.mfit, self.mfit)
@@ -163,34 +174,38 @@ class SASFitController(Controller):
         """
         Run problem
         """
+
         self.lib.SASFITmrqmin(
             self.x_ptr,  # x values
             self.y_ptr,  # measured y
             self.sig_ptr,  # standard dev
             self.yfit_ptr,  # model prediction
-            c_int(self.ndata), # n data
+            c_int(self.ndata),  # n data
             self.a_arr,  # initial params
             self.atry_arr,
             self.da_arr,
             self.beta_arr,
-            c_int(self.ma), # n total params
-            self.lista_arr, # parameter–index mapping array to keep track of which parameters are being fitted
-            c_int(self.mfit), # n fitted params
+            c_int(self.ma),  # n total params
+            self.lista_arr,  # param idx mapping arr, track which params fitted
+            c_int(self.mfit),  # n fitted params
             self.covar_mat,  # working space array
             self.alpha_mat,  # working space array
             self.oneda_mat,  # working space array
-            byref(self.chisq),  # Pointer to current chi‑square value
-            _________,  # FIXME: this should be the model function , e.g., the form factor
-            byref(self.alamda),  # Levenberg–Marquardt control parameter
-            c_int(self.error_type), # 0-4
+            byref(self.chisq),  # Pointer to current chi square value
+            self.make_funcs_wrapper(self.cost_func),  # the model function
+            byref(self.alamda),  # Levenberg Marquardt control parameter
+            c_int(self.error_type),  # 0 to 4
             byref(c_bool(False)),  # Set to TRUE if anything goes wrong
         )
+
+        self._popt = self.ptr_to_numpy(self.a_arr, self.ma)
 
     def make_matrix_float(self, rows, cols):
         """
         Helper to create float** matrices
         Returns (mat_ptr, row_buffers) where:
-        - mat_ptr is POINTER(POINTER(c_float)) suitable for passing to C as float**
+        - mat_ptr is POINTER(POINTER(c_float)) suitable for passing
+          to C as float**
         - row_buffers is a list you must KEEP referenced so memory isn't GC'd
         """
         RowArray = c_float * cols
@@ -199,6 +214,55 @@ class SASFitController(Controller):
         MatArray = POINTER(c_float) * rows
         mat = MatArray(*[row_buffers[r] for r in range(rows)])
         return mat, row_buffers
-    
+
+    def make_funcs_wrapper(self, cost_func):
+        def funcs_wrapper(x_i, a_ptr, ymod_ptr, dyda_ptr):
+            # need this for getting the right jac later
+            idx = int(np.where(self.data_x == x_i)[0][0])
+
+            # extract params correctly
+            params = [float(a_ptr[i]) for i in range(self.n_params)]
+
+            # compute model
+            y = float(
+                cost_func.problem.eval_model(x=np.array(x_i), params=params)
+            )
+            ymod_ptr[0] = y
+
+            # compute jacobian
+            jac = cost_func.jac_res(params)
+
+            for i in range(self.n_params):
+                dyda_ptr[i] = np.asarray(jac[idx][i], dtype=np.float32)
+
+            return y
+
+        return FUNCS_T(funcs_wrapper)
+
+    def ptr_to_numpy(self, ptr, length) -> np.ndarray:
+        # Create a 1D numpy array that views the same memory
+        return np.ctypeslib.as_array(ptr, shape=(length,))
+
     def cleanup(self):
-        return
+        # max_iters_messages = [
+        #     "maximum number of iterations",
+        #     "iteration limit reached",
+        #     "iterations reached limit",
+        # ]
+
+        # if self.result.success:
+        #     self.flag = 0
+        # elif any(
+        #     message in self.result.message.lower()
+        #     for message in max_iters_messages
+        # ):
+        #     self.flag = 1
+        # else:
+        #     self.flag = 2
+
+        # if "nfev" in self.result:
+        #     self.func_evals = self.result.nfev
+        # if "nit" in self.result:
+        #     self.iteration_count = self.result.nit
+        self.flag = 0
+        self.final_params = self._popt
