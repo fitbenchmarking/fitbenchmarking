3f1fe801601767024ec5089ccdc8167065f3a315
Sam Cunningham <Sam.Cunningham@stfc.ac.uk>
2026-04-23 14:58:05 +0100

    Added the ability to see various metrics and run status in a tooltip over the results tables (#1596)
    * added hover text to results table
    
    * update unit tests to expect the new hover text formatting
    
    * add default value when retrieving status from checkpoint
    
    * improve docstrings
    
    * Make error flag mappings list globally accessible to reduce duplication
    
    * switch to using const dict instead of function call
    
    * update expected html for testing tables
    
    * Fix spelling mistake in docstring for get_hover_str_dict
    
    Co-authored-by: Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
    
    * fix capitalisation in docstring for get_hover_text
    
    Co-authored-by: Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
    
    * Return hover text already in the correct format
    
    Keys are not needed for setting the hover text, so we can strip them
    before display
    
    * move comment into correct function
    
    * improve tooltip string readability
    
    * use ERROR_FLAG_MAPPINGS more widely
    
    * pretty print available error keys
    
    * update formatting of generated docstring
    
    * switch from \n to rst newline character
    
    * use <br> for newline chars in dosctring
    
    * use \a instead for newline
    
    * remove extra escape char
    
    * format docstring using rst list
    
    * format docstring using rst 'field list'
    
    * remove redundant import
    
    * re-add required import for pandas.io.formats.style
    
    * update expected test data to support merged changes
    
    ---------
    
    Co-authored-by: Letizia Protopapa <letizia.protopapa@stfc.ac.uk>

--- fitbenchmarking/utils/misc.py
+++ fitbenchmarking/utils/misc.py
@@ -11,6 +11,19 @@ from fitbenchmarking.utils.log import get_logger
 LOGGER = get_logger()
 
 
+ERROR_FLAG_MAPPINGS = {
+    0: "Successfully converged",
+    1: "Software reported maximum number of iterations exceeded",
+    2: "Software run but didn't converge to solution",
+    3: "Software raised an exception",
+    4: "Solver doesn't support bounded problems",
+    5: "Solution doesn't respect parameter bounds",
+    6: "Solver has exceeded maximum allowed runtime",
+    7: "Validation of the provided options failed",
+    8: "Confidence in fit could not be calculated",
+}
+
+
 def get_problem_files(data_dir):
     """
     Gets all the problem definition files from the specified problem
