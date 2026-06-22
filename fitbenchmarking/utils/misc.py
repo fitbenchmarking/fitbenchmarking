47feb706feef8d3153ee628af137884690102c31
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2026-06-22 10:52:46 +0100

    Removed polyfill lines from html files (#1622)
    * Remove polyfill lines
    
    * Remove mathjax js file and get it from cdn
    
    ---------
    
    Co-authored-by: Jessica Huntley <79837359+jess-farmer@users.noreply.github.com>

--- fitbenchmarking/utils/misc.py
+++ fitbenchmarking/utils/misc.py
@@ -93,7 +93,6 @@ def get_js(options, working_directory):
     js_path = os.path.relpath(local_js_dir, working_directory)
     js_dict = {
         "dropdown": os.path.join(js_path, "dropdown.js"),
-        "mathjax": os.path.join(js_path, "tex-mml-chtml.js"),
         "table": os.path.join(js_path, "table.js"),
     }
 
