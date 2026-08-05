a1afb65ac4100f22bac3ae893b57d311547a6d0c
Letizia Protopapa <letizia.protopapa@stfc.ac.uk>
2026-08-05 14:01:48 +0100

    remove tracker changes, to be dealt with in another PR

--- fitbenchmarking/core/fitting_benchmarking.py
+++ fitbenchmarking/core/fitting_benchmarking.py
@@ -560,7 +560,6 @@ class Fit:
         num_runs = self._options.num_runs
         energy = np.nan
         tracker = self._emissions_tracker
-        tracker_started = False
         tracker_stopped = False
 
         try:
@@ -569,7 +568,6 @@ class Fit:
                 controller.prepare()
                 if tracker:
                     tracker.start_task()
-                    tracker_started = True
                     runtimes = timeit.Timer(stmt=controller.execute).repeat(
                         num_runs, 1
                     )
@@ -652,10 +650,8 @@ class Fit:
         # Reset the controller timer once exceptions have been handled
         controller.timer.reset()
 
-        # Ensure emissions tracker has been stopped if energy not set. The
-        # task is only stopped if it was started, as the fit may have raised
-        # (e.g. a validation error) before it was started.
-        if self._emissions_tracker and tracker_started and not tracker_stopped:
+        # ensure emissions tracker has been stopped if energy not set
+        if self._emissions_tracker and not tracker_stopped:
             _ = self._emissions_tracker.stop_task()
 
         if controller.flag in [3, 6, 7]:
