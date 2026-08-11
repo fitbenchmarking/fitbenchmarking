a8a3e7dc2ef6de843145a1331e625e892859e189
SamC-STFC <sam.cunningham@stfc.ac.uk>
2026-08-11 08:13:45 +0000

    update js script to use const for indexing customdata

--- fitbenchmarking/results_processing/scripts/compare_scatter/handle_link.js
+++ fitbenchmarking/results_processing/scripts/compare_scatter/handle_link.js
@@ -2,7 +2,8 @@ function(data){
     if (!data) {
         return null;
     }
-    var newpath = data.points?.[0]?.customdata?.[3];
+    const DATA_REPORT_FILE_INDEX = 3;
+    var newpath = data.points?.[0]?.customdata?.[DATA_REPORT_FILE_INDEX];
     if (typeof(newpath) !== "undefined") {
         window.parent.postMessage({path:newpath},"*");
     }
