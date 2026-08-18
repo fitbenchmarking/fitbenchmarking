e73844b9533485e5518f08143077511509246040
SamC-STFC <sam.cunningham@stfc.ac.uk>
2026-08-18 11:08:30 +0000

    only run flakefinder on flaky test suites, and reduce n runs

--- ci/system_tests_default.sh
+++ ci/system_tests_default.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # ======= System Test =============== #
-pytest fitbenchmarking/systests --test-type default --junit-xml test-results/default_system_pytest.xml --durations=0 --flake-finder
+pytest fitbenchmarking/systests --test-type default --junit-xml test-results/default_system_pytest.xml --durations=0
 
 exit $?
--- ci/system_tests_mantid.sh
+++ ci/system_tests_mantid.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # ======= System Test =============== #
-pytest fitbenchmarking/systests --test-type mantid --junit-xml test-results/full_system_pytest.xml --durations=0 --flake-finder
+pytest fitbenchmarking/systests --test-type mantid --junit-xml test-results/full_system_pytest.xml --durations=0 --flake-finder --flake-runs=5
 
 exit $?
